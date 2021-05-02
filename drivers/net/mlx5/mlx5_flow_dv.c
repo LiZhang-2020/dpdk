LiZhang-2020/dpdk

commit d9106cdd24a0c902d699eebee2147d417bc2b970

net/mlx5: add translation for CT action
When creating a flow with this action context for CT, it needs to be
translated in 2 levels.
First, retrieve from action context to RTE_FLOW action.
Second, translate it to the correct DR action with traffic direction.

Before using the DR action in a flow, the CT context should be
available to use in the hardware.

Signed-off-by: Bing Zhao <bingz@nvidia.com>

drivers/net/mlx5/mlx5_flow_dv.c
@@ -11988,6 +11988,7 @@ flow_dv_translate_create_conntrack(struct rte_eth_dev *dev,
 		return rte_flow_error_set(error, EBUSY,
 					  RTE_FLOW_ERROR_TYPE_ACTION, NULL,
 					  "Failed to update CT");
+	ct->is_original = !!pro->is_original_dir;
 	return idx;
 }
 
@@ -12146,6 +12147,8 @@ flow_dv_translate(struct rte_eth_dev *dev,
 		int action_type = actions->type;
 		const struct rte_flow_action *found_action = NULL;
 		uint32_t jump_group = 0;
+		uint32_t ct_idx;
+		struct mlx5_aso_ct_action *ct;
 
 		if (!mlx5_flow_os_action_supported(action_type))
 			return rte_flow_error_set(error, ENOTSUP,
@@ -12622,6 +12625,21 @@ flow_dv_translate(struct rte_eth_dev *dev,
 					(dev, mhdr_res, actions, attr, error))
 				return -rte_errno;
 			action_flags |= MLX5_FLOW_ACTION_MODIFY_FIELD;
+			break;
+		case RTE_FLOW_ACTION_TYPE_CONNTRACK:
+			ct_idx = (uint32_t)(uintptr_t)action->conf;
+			ct = flow_aso_ct_get_by_idx(dev, ct_idx);
+			if (mlx5_aso_ct_available(priv->sh, ct))
+				return -rte_errno;
+			if (ct->is_original)
+				dev_flow->dv.actions[actions_n] =
+							ct->dr_action_orig;
+			else
+				dev_flow->dv.actions[actions_n] =
+							ct->dr_action_rply;
+			__atomic_fetch_add(&ct->refcnt, 1, __ATOMIC_RELAXED);
+			actions_n++;
+			action_flags |= MLX5_FLOW_ACTION_CT;
 			break;
 		case RTE_FLOW_ACTION_TYPE_END:
 			actions_end = true;

drivers/net/mlx5/mlx5.h
@@ -1657,6 +1657,9 @@ int mlx5_aso_ct_wait_ready(struct mlx5_dev_ctx_shared *sh,
 int mlx5_aso_ct_query_by_wqe(struct mlx5_dev_ctx_shared *sh,
 			     struct mlx5_aso_ct_action *ct,
 			     struct rte_flow_action_conntrack *profile);
+int mlx5_aso_ct_available(struct mlx5_dev_ctx_shared *sh,
+			  struct mlx5_aso_ct_action *ct);
+
 
 /* mlx5_sft.c */
 

drivers/net/mlx5/mlx5_flow.c
@@ -3547,6 +3547,15 @@ flow_shared_actions_translate(struct rte_eth_dev *dev,
 				break;
 			}
 			/* Fall-through */
+		case MLX5_SHARED_ACTION_TYPE_CT:
+			if (priv->sh->ct_aso_en) {
+				translated[shared->index].type =
+					RTE_FLOW_ACTION_TYPE_CONNTRACK;
+				translated[shared->index].conf =
+							 (void *)(uintptr_t)idx;
+				break;
+			}
+			/* Fall-through */
 		default:
 			mlx5_free(translated);
 			return rte_flow_error_set

drivers/net/mlx5/mlx5_flow.h
@@ -249,6 +249,7 @@ enum mlx5_feature_name {
 #define MLX5_FLOW_ACTION_TUNNEL_MATCH (1ull << 38)
 #define MLX5_FLOW_ACTION_MODIFY_FIELD (1ull << 39)
 #define MLX5_FLOW_ACTION_SFT (1ull << 40)
+#define MLX5_FLOW_ACTION_CT (1ull << 41)
 
 #define MLX5_FLOW_FATE_ACTIONS \
 	(MLX5_FLOW_ACTION_DROP | MLX5_FLOW_ACTION_QUEUE | \
@@ -1054,6 +1055,10 @@ struct mlx5_shared_action_rss {
 };
 
 struct rte_flow_shared_action {
+	uint32_t id;
+};
+
+struct rte_flow_action_ctx {
 	uint32_t id;
 };
 

drivers/net/mlx5/mlx5_flow_aso.c
@@ -1450,3 +1450,29 @@ data_handle:
 		mlx5_aso_ct_obj_analyze(profile, out_data);
 	return ret;
 }
+
+int
+mlx5_aso_ct_available(struct mlx5_dev_ctx_shared *sh,
+		      struct mlx5_aso_ct_action *ct)
+{
+	struct mlx5_aso_ct_pools_mng *mng = sh->ct_mng;
+	uint32_t poll_cqe_times = MLX5_CT_POLL_WQE_CQE_TIMES;
+	uint8_t state = __atomic_load_n(&ct->state, __ATOMIC_RELAXED);
+
+	if (state == ASO_CONNTRACK_FREE) {
+		rte_errno = ENXIO;
+		return -rte_errno;
+	} else if (state == ASO_CONNTRACK_READY || state == ASO_CONNTRACK_QUERY)
+		return 0;
+	do {
+		mlx5_aso_ct_completion_handle(mng);
+		state = __atomic_load_n(&ct->state, __ATOMIC_RELAXED);
+		if (state == ASO_CONNTRACK_READY ||
+		    state == ASO_CONNTRACK_QUERY)
+			return 0;
+		/* Waiting for CQE ready, consider should block or sleep.  */
+		usleep(MLX5_ASO_WQE_CQE_RESPONSE_DELAY);
+	} while (--poll_cqe_times);
+	rte_errno = EBUSY;
+	return -rte_errno;
+}

commit 8fe5e911e449ec38ef5ad699b417439f6f58cc96

net/mlx5: fix the errno in CT translation
In flow translation, when there is an error for the conntrack
action, the rte_errno will be set and return directly. The other
fields of the "struct rte_flow_error" are not set and there is
a risk if the caller wants to access the message by the pointer.

All the error return branch should make sure that the fields of the
whole structure are set properly before return.

Fixes: d9106cdd24a0 ("net/mlx5: add translation for CT action")

Signed-off-by: Bing Zhao <bingz@nvidia.com>

drivers/net/mlx5/mlx5_flow_dv.c
@@ -12854,12 +12854,16 @@ flow_dv_translate(struct rte_eth_dev *dev,
 		case RTE_FLOW_ACTION_TYPE_CONNTRACK:
 			owner_idx = (uint32_t)(uintptr_t)action->conf;
 			ct = flow_aso_ct_get_by_idx(dev, owner_idx);
-			if (!ct) {
-				rte_errno = EINVAL;
-				return -rte_errno;
-			}
+			if (!ct)
+				return rte_flow_error_set(error, EINVAL,
+						RTE_FLOW_ERROR_TYPE_ACTION,
+						NULL,
+						"Failed to get CT object.");
 			if (mlx5_aso_ct_available(priv->sh, ct))
-				return -rte_errno;
+				return rte_flow_error_set(error, rte_errno,
+						RTE_FLOW_ERROR_TYPE_ACTION,
+						NULL,
+						"CT is unavailable.");
 			if (ct->is_original)
 				dev_flow->dv.actions[actions_n] =
 							ct->dr_action_orig;