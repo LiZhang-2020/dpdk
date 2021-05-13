LiZhang-2020/dpdk

commit 49b175698dd9806752f4fd017a8f687d4c1c1e36

ethdev: introduce sft lib
Defines RTE SFT (Stateful Flow Table) APIs for Stateful Flow Table library.

Currently, DPDK enables only stateless offloading, using the rte_flow.
stateless means that each packet is handled without any knowledge of
previous or future packets.

As we look at the industry, there is much demand to save a context across
packets that belong to a connection.

Examples for such applications:
- Next-generation firewalls
- Intrusion detection/prevention systems (IDS/IPS): Suricata, snort
- SW/Virtual Switching: OVS
The goals of the SFT library:
- Accelerate flow recognition & its context retrieval for further
  lookaside processing.
- Enable context-aware flow handling offload.

The solution suggested is to create a lib that will enable saving states
between different packets that belong to the same connection.

The solution will also enable better HW abstraction than the one we get
from using the rte_flow. The reason for this is that saving states is
not atomic action like we have in rte_flow and also can't be done fully
in HW (The first packets must be seen by the application).
Saying the above this lib is based on interacting with the rte_flow but it
doesn't replace it or encapsulate it.

Key design points.
- The SFT should offload as much as possible to HW.
- The SFT is designed to work alongside the rte_flow.
- The SFT has its own ops that the PMD needs to implement.
- The SFT works on 5 tuple + zone (a user-defined value)

Basic usage flow:
1. Application insert a flow that matches all eth traffic and have
   sft action along with jump action. (in the future this jump can be
   avoided and in doing so saving some jumps,
   but for the most generic and complete solution we think
   that allow the application full control of the packet process using
   rte_flow is better.)
2. Application insert a flow in the target group that matches the packet
   state. Based on this state the application performs the needed

lib/librte_ethdev/rte_sft.c
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020 Mellanox Technologies, Ltd
+ */
+
+
+#include "rte_sft.h"
+#include "rte_sft_driver.h"
+
+/* Placeholder for RTE SFT library APIs implementation */

lib/librte_ethdev/rte_sft_driver.h
@@ -1,0 +1,227 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020 Mellanox Technologies, Ltd
+ */
+
+#ifndef RTE_SFT_DRIVER_H_
+#define RTE_SFT_DRIVER_H_
+
+/**
+ * @file
+ * RTE generic SFT API (driver side)
+ *
+ * This file provides implementation helpers for internal use by PMDs, they
+ * are not intended to be exposed to applications and are not subject to ABI
+ * versioning.
+ */
+
+#include <stdint.h>
+
+#include "rte_ethdev.h"
+#include "rte_ethdev_driver.h"
+#include "rte_sft.h"
+#include "rte_flow.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct rte_sft_entry;
+
+__rte_internal int
+rte_sft_error_set(struct rte_sft_error *error, int code,
+		  enum rte_sft_error_type type, const void *cause,
+                  const char *message);
+
+#define RTE_SFT_STATE_INVALID (0)
+#define RTE_SFT_STATE_FLAG_FID_VALID (1 << 0)
+#define RTE_SFT_STATE_FLAG_ZONE_VALID (1 << 1)
+#define RTE_SFT_STATE_FLAG_FLOW_MISS (1 << 2)
+
+#define RTE_SFT_MISS_TCP_FLAGS (1 << 0)
+
+RTE_STD_C11
+struct rte_sft_decode_info {
+	union {
+		uint32_t fid; /**< The fid value. */
+		uint32_t zone; /**< The zone value. */
+	};
+	uint32_t state;
+	/**< Flags that mark the packet state. see RTE_SFT_STATE_FLAG_*. */
+};
+
+/**
+ * @internal
+ * Init the SFT pmd
+ *
+ * @param dev
+ *   ethdev handle of port.
+ * @param nb_queue
+ *   Number of queues.
+ * @param data_len
+ *   The length of the data in uint32_t increments.
+ * @param error[out]
+ *   Verbose of the error.
+ *
+ * @return
+ *   Negative errno value on error, 0 on success.
+ */
+typedef int (*sft_start_t) (struct rte_eth_dev *dev, uint16_t nb_queue,
+			   uint16_t data_len, struct rte_sft_error *error);
+
+/**
+ * @internal
+ * close the SFT pmd
+ *
+ * @param dev
+ *   ethdev handle of port.
+ * @param error[out]
+ *   Verbose of the error.
+ *
+ * @return
+ *   Negative errno value on error, 0 on success.
+ */
+typedef int (*sft_stop_t) (struct rte_eth_dev *dev, struct rte_sft_error *error);
+
+/**
+ * @internal
+ * Insert a flow to the SFT HW component.
+ *
+ * @param dev
+ *   ethdev handle of port.
+ * @param fid
+ *   Flow ID.
+ * @param queue
+ *   The sft working queue.
+ * @param pattern
+ *   The matching pattern.
+ * @param miss_conditions
+ *   The conditions that forces a miss even if the 5 tuple was matched
+ *   see RTE_SFT_MISS_*.
+ * @param actions
+ *   Set pf actions to apply in case the flow was hit. If no terminating action
+ *   (queue, rss, drop, port) was given, the terminating action should be taken
+ *   from the flow that resulted in the SFT.
+ * @param miss_actions
+ *   Set pf actions to apply in case the flow was hit. but the miss conditions
+ *   were hit. (6 tuple match but tcp flags are on) If no terminating action
+ *   (queue, rss, drop, port) was given, the terminating action should be taken
+ *   from the flow that resulted in the SFT.
+ * @param data
+ *   The application data to attached to the flow.
+ * @param data_len
+ *   The length of the data in uint32_t increments.
+ * @param state
+ *   The application state to set.
+ * @param error[out]
+ *   Verbose of the error.
+ *
+ * @return
+ *   Pointer to sft_entry in case of success, null otherwise and rte_sft_error
+ *   is set.
+ */
+typedef struct rte_sft_entry *(*sft_entry_create_t)
+		(struct rte_eth_dev *dev, uint32_t fid, uint16_t queue,
+		 const struct rte_flow_item *pattern, uint64_t miss_conditions,
+		 const struct rte_flow_action *actions,
+		 const struct rte_flow_action *miss_actions,
+		 const uint32_t *data, uint16_t data_len, uint8_t state,
+		 struct rte_sft_error *error);
+
+/**
+ * @internal
+ * Modify the state and the data of SFT flow in HW component.
+ *
+ * @param dev
+ *   ethdev handle of port.
+ * @param entry
+ *   The entry to modify.
+ * @param queue
+ *   The sft working queue.
+ * @param entry
+ *   The entry to modify.
+ * @param data
+ *   The application data to attached to the flow.
+ * @param data_len
+ *   The length of the data in uint32_t increments.
+ * @param state
+ *   The application state to set.
+ * @param error[out]
+ *   Verbose of the error.
+ *
+ * @return
+ *   Negative errno value on error, 0 on success.
+ */
+typedef int (*sft_entry_modify_t)(struct rte_eth_dev *dev, uint16_t queue,
+				  struct rte_sft_entry *entry,
+				  const uint32_t *data, uint16_t data_len,
+				  uint8_t state, struct rte_sft_error *error);
+
+/**
+ * @internal
+ * Destroy SFT flow in HW component.
+ *
+ * @param dev
+ *   ethdev handle of port.
+ * @param entry
+ *   The entry to destroy.
+ * @param queue
+ *   The sft working queue.
+ * @param error[out]
+ *   Verbose of the error.
+ *
+ * @return
+ *   Negative errno value on error, 0 on success.
+ */
+typedef int (*sft_entry_destroy_t)(struct rte_eth_dev *dev,
+				   struct rte_sft_entry *entry, uint16_t queue,
+				   struct rte_sft_error *error);
+
+/**
+ * @internal
+ * Decode sft state and FID from mbuf.
+ *
+ * @param dev
+ *   ethdev handle of port.
+ * @param entry
+ *   The entry to modify.
+ * @param queue
+ *   The sft working queue.
+ * @param mbuf
+ *   The input mbuf.
+ * @param info[out]
+ *   The decoded sft data.
+ * @param error[out]
+ *   Verbose of the error.
+ *
+ * @return
+ *   Negative errno value on error, 0 on success.
+ */
+typedef int (*sft_entry_decode_t)(struct rte_eth_dev *dev, uint16_t queue,
+				  struct rte_mbuf *mbuf,
+				  struct rte_sft_decode_info *info,
+				  struct rte_sft_error *error);
+
+/**
+ * Generic sft operations structure implemented and returned by PMDs.
+ *
+ * If successful, this operation must result in a pointer to a PMD-specific.
+ *
+ * See also rte_sft_ops_get().
+ *
+ * These callback functions are not supposed to be used by applications
+ * directly, which must rely on the API defined in rte_sft.h.
+ */
+struct rte_sft_ops {
+	sft_start_t sft_start;
+	sft_stop_t sft_stop;
+	sft_entry_create_t sft_create_entry;
+	sft_entry_modify_t sft_entry_modify;
+	sft_entry_destroy_t sft_entry_destroy;
+	sft_entry_decode_t sft_entry_decode;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RTE_SFT_DRIVER_H_ */

lib/librte_ethdev/meson.build
@@ -8,6 +8,7 @@ sources = files('ethdev_private.c',
 	'rte_ethdev.c',
 	'rte_flow.c',
 	'rte_mtr.c',
+	'rte_sft.c',
 	'rte_tm.c')
 
 headers = files('rte_ethdev.h',
@@ -23,6 +24,8 @@ headers = files('rte_ethdev.h',
 	'rte_flow_driver.h',
 	'rte_mtr.h',
 	'rte_mtr_driver.h',
+	'rte_sft.h',
+	'rte_sft_driver.h',
 	'rte_tm.h',
 	'rte_tm_driver.h')
 

lib/librte_ethdev/rte_ethdev_driver.h
@@ -500,6 +500,9 @@ typedef int (*eth_tm_ops_get_t)(struct rte_eth_dev *dev, void *ops);
 typedef int (*eth_mtr_ops_get_t)(struct rte_eth_dev *dev, void *ops);
 /**< @internal Get Traffic Metering and Policing (MTR) operations */
 
+typedef int (*eth_sft_ops_get_t)(struct rte_eth_dev *dev, void *ops);
+/**< @internal Get Stateful Flow Table (SFT) operations. */
+
 typedef int (*eth_get_dcb_info)(struct rte_eth_dev *dev,
 				 struct rte_eth_dcb_info *dcb_info);
 /**< @internal Get dcb information on an Ethernet device */
@@ -888,6 +891,9 @@ struct eth_dev_ops {
 
 	eth_mtr_ops_get_t mtr_ops_get;
 	/**< Get Traffic Metering and Policing (MTR) operations. */
+
+	eth_sft_ops_get_t sft_ops_get;
+	/**< @internal Get Stateful Flow Table (SFT) operations. */
 
 	eth_pool_ops_supported_t pool_ops_supported;
 	/**< Test if a port supports specific mempool ops */

lib/librte_ethdev/version.map
@@ -240,6 +240,25 @@ EXPERIMENTAL {
 	rte_flow_get_restore_info;
 	rte_flow_tunnel_action_decap_release;
 	rte_flow_tunnel_item_release;
+	rte_sft_drain_mbuf;
+	rte_sft_fini;
+	rte_sft_flow_activate;
+	rte_sft_flow_create;
+	rte_sft_flow_destroy;
+	rte_sft_flow_get_client_obj;
+	rte_sft_flow_get_status;
+	rte_sft_flow_query;
+	rte_sft_flow_set_aging;
+	rte_sft_flow_set_client_obj;
+	rte_sft_flow_set_data;
+	rte_sft_flow_set_offload;
+	rte_sft_flow_set_state;
+	rte_sft_flow_touch;
+	rte_sft_init;
+	rte_sft_process_mbuf;
+	rte_sft_process_mbuf_with_zone;
+
+
 };
 
 INTERNAL {

commit 89f4be274100626d820dd4c3fc4b886ea4f727ad

ethdev: fix sft driver API
Update prototypes for sft_entry_create_t and sft_entry_decode_t.

Fixes: 49b175698dd9 ("ethdev: introduce sft lib")

Signed-off-by: Gregory Etelson <getelson@nvidia.com>

drivers/net/mlx5/mlx5_sft.c
@@ -280,13 +280,12 @@ static int mlx5_sft_stop(struct rte_eth_dev *dev, struct rte_sft_error *error)
 }
 
 static struct rte_sft_entry *
-mlx5_sft_entry_create(struct rte_eth_dev *dev, uint32_t fid, uint16_t queue,
-		      const struct rte_flow_item *pattern,
-		      uint64_t miss_conditions,
-		      const struct rte_flow_action *actions,
-		      const struct rte_flow_action *miss_actions,
-		      const uint32_t *data, uint16_t data_len,
-		      uint8_t state, struct rte_sft_error *error)
+mlx5_sft_entry_create(struct rte_eth_dev *dev, uint32_t fid, uint32_t zone,
+		      uint16_t queue, struct rte_flow_item *pattern,
+		      uint64_t miss_conditions, struct rte_flow_action *actions,
+		      struct rte_flow_action *miss_actions,
+		      const uint32_t *data, uint16_t data_len, uint8_t state,
+		      bool initiator, struct rte_sft_error *error)
 {
 	struct mlx5_priv *priv = dev->data->dev_private;
 	struct mlx5_dev_ctx_shared *sh = priv->sh;
@@ -371,6 +370,8 @@ mlx5_sft_entry_create(struct rte_eth_dev *dev, uint32_t fid, uint16_t queue,
 	const struct rte_flow_action *l_actions = actions;
 	const struct rte_flow_action *lm_actions = miss_actions;
 	RTE_SET_USED(miss_conditions);
+	RTE_SET_USED(zone);
+	RTE_SET_USED(initiator);
 
 	if (!priv->config.sft_en) {
 		rte_sft_error_set(error, ENOTSUP,
@@ -595,7 +596,7 @@ static int mlx5_sft_entry_destroy(struct rte_eth_dev *dev,
 }
 
 static int mlx5_sft_entry_decode(struct rte_eth_dev *dev, uint16_t queue,
-				 struct rte_mbuf *mbuf,
+				 const struct rte_mbuf *mbuf,
 				 struct rte_sft_decode_info *info,
 				 struct rte_sft_error *error)
 {

lib/librte_ethdev/rte_sft.c
@@ -491,16 +491,12 @@ sft_mbuf_decode(uint16_t queue, const struct rte_mbuf *mbuf,
 	int ret;
 	struct rte_eth_dev *dev = &rte_eth_devices[mbuf->port];
 	const struct rte_sft_ops *sft_ops = sft_ops_get(dev);
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wcast-qual"
-	struct rte_mbuf *m = (typeof(m))mbuf;
-#pragma GCC diagnostic pop
 
 	if (!sft_ops) {
 		info->state = 0;
 		return 0;
 	}
-	ret = sft_ops->sft_entry_decode(dev, queue, m, info, error);
+	ret = sft_ops->sft_entry_decode(dev, queue, mbuf, info, error);
 	if (ret)
 		return ret;
 	else if (info->state && (info->state & ~RTE_SFT_STATE_MASK))
@@ -882,11 +878,10 @@ sft_create_entry(struct rte_eth_dev *dev[2], const struct rte_sft_ops *ops[2],
 	sft_entry_create_t create_entry = ops[dir]->sft_create_entry;
 	bool initiator = !dir;
 
-	RTE_SET_USED(initiator);
-	se = create_entry(dev[dir], entry->fid, entry->queue,
+	se = create_entry(dev[dir], entry->fid, entry->zone, entry->queue,
 			  hit_pattern[dir], miss_conditions, hit_actions[dir],
 			  miss_actions[dir], entry->data, SFT_DATA_LEN,
-			  entry->app_state, error);
+			  entry->app_state, initiator, error);
 	entry->sft_entry[dir] = se;
 	if (entry->sft_entry[dir]) {
 		ret = 0;

lib/librte_ethdev/rte_sft_driver.h
@@ -135,13 +135,12 @@ typedef int (*sft_stop_t) (struct rte_eth_dev *dev, struct rte_sft_error *error)
  *   is set.
  */
 typedef struct rte_sft_entry *(*sft_entry_create_t)
-	(struct rte_eth_dev *dev, uint32_t fid, uint16_t queue,
-	 const struct rte_flow_item *pattern,
-	 uint64_t miss_conditions,
-	 const struct rte_flow_action *actions,
-	 const struct rte_flow_action *miss_actions,
-	 const uint32_t *data, uint16_t data_len,
-	 uint8_t state, struct rte_sft_error *error);
+	(struct rte_eth_dev *dev, uint32_t fid, uint32_t zone, uint16_t queue,
+	 struct rte_flow_item *pattern, uint64_t miss_conditions,
+	 struct rte_flow_action *actions,
+	 struct rte_flow_action *miss_actions, const uint32_t *data,
+	 uint16_t data_len, uint8_t state, bool initiator,
+	 struct rte_sft_error *error);
 
 /**
  * @internal
@@ -213,7 +212,7 @@ typedef int (*sft_entry_destroy_t)(struct rte_eth_dev *dev,
  *   Negative errno value on error, 0 on success.
  */
 typedef int (*sft_entry_decode_t)(struct rte_eth_dev *dev, uint16_t queue,
-				  struct rte_mbuf *mbuf,
+				  const struct rte_mbuf *mbuf,
 				  struct rte_sft_decode_info *info,
 				  struct rte_sft_error *error);
 