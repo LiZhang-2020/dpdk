84619ef32c902d627040ed8fca29fdfa3e15f239
rongwei liu <rongweil@nvidia.com>
2021-05-25 05:41:20 +0300

    app/testpmd: support VXLAN last 8-bits field matching
    Add a new testpmd pattern field 'last_rsvd' that supports the
    last 8-bits matching of VXLAN header.
    
    The examples for the "last_rsvd" pattern field are as below:
    
    1. ...pattern eth / ipv4 / udp / vxlan last_rsvd is 0x80 / end ...
    
    This flow will exactly match the last 8-bits to be 0x80.
    
    1. ...pattern eth / ipv4 / udp / vxlan last_rsvd spec 0x80
    vxlan mask 0x80 / end ...
    
    This flow will only match the MSB of the last 8-bits to be 1.
    
    Signed-off-by: rongwei liu <rongweil@nvidia.com>

--- app/test-pmd/cmdline_flow.c
+++ app/test-pmd/cmdline_flow.c
@@ -207,6 +207,7 @@ enum index {
 	ITEM_SCTP_CKSUM,
 	ITEM_VXLAN,
 	ITEM_VXLAN_VNI,
+	ITEM_VXLAN_LAST_RSVD,
 	ITEM_E_TAG,
 	ITEM_E_TAG_GRP_ECID_B,
 	ITEM_NVGRE,
@@ -1145,6 +1146,7 @@ static const enum index item_sctp[] = {
 
 static const enum index item_vxlan[] = {
 	ITEM_VXLAN_VNI,
+	ITEM_VXLAN_LAST_RSVD,
 	ITEM_NEXT,
 	ZERO,
 };
@@ -2858,6 +2860,13 @@ static const struct token token_list[] = {
 		.help = "VXLAN identifier",
 		.next = NEXT(item_vxlan, NEXT_ENTRY(UNSIGNED), item_param),
 		.args = ARGS(ARGS_ENTRY_HTON(struct rte_flow_item_vxlan, vni)),
+	},
+	[ITEM_VXLAN_LAST_RSVD] = {
+		.name = "last_rsvd",
+		.help = "VXLAN last reserved bits",
+		.next = NEXT(item_vxlan, NEXT_ENTRY(UNSIGNED), item_param),
+		.args = ARGS(ARGS_ENTRY_HTON(struct rte_flow_item_vxlan,
+					     rsvd1)),
 	},
 	[ITEM_E_TAG] = {
 		.name = "e_tag",
