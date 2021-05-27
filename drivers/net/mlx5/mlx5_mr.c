cae9647e337a099f48005b51ebe881e17bae924e
Feifei Wang <feifei.wang2@arm.com>
2021-05-27 17:48:06 +0800

    net/mlx5: fix r/w lock usage in DMA unmap
    [ upstream commit 1c196da274b82e6f1642bde7b1c407a2066caee1 ]
    
    For mlx5 DMA unmap, write lock should be used for rebuilding memory
    region cache table rather than read lock.
    
    Fixes: 989e999d9305 ("net/mlx5: support PCI device DMA map and unmap")
    
    Signed-off-by: Feifei Wang <feifei.wang2@arm.com>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>
    
    Signed-off-by: Xiaoyu Min <jackmin@nvidia.com>

--- drivers/net/mlx5/mlx5_mr.c
+++ drivers/net/mlx5/mlx5_mr.c
@@ -302,10 +302,10 @@ mlx5_net_dma_unmap(struct rte_device *rte_dev, void *addr,
 	}
 	priv = dev->data->dev_private;
 	sh = priv->sh;
-	rte_rwlock_read_lock(&sh->share_cache.rwlock);
+	rte_rwlock_write_lock(&sh->share_cache.rwlock);
 	mr = mlx5_mr_lookup_list(&sh->share_cache, &entry, (uintptr_t)addr);
 	if (!mr) {
-		rte_rwlock_read_unlock(&sh->share_cache.rwlock);
+		rte_rwlock_write_unlock(&sh->share_cache.rwlock);
 		DRV_LOG(WARNING, "address 0x%" PRIxPTR " wasn't registered to device %s",
 			(uintptr_t)addr, rte_dev->name);
 		rte_errno = EINVAL;
@@ -329,7 +329,7 @@ mlx5_net_dma_unmap(struct rte_device *rte_dev, void *addr,
 	DEBUG("broadcasting local cache flush, gen=%d",
 	      sh->share_cache.dev_gen);
 	rte_smp_wmb();
-	rte_rwlock_read_unlock(&sh->share_cache.rwlock);
+	rte_rwlock_write_unlock(&sh->share_cache.rwlock);
 	return 0;
 }
 
