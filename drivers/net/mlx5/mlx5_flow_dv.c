LiZhang-2020/dpdk

commit 9af445c6d6c0d8e0083b8737bb3ee05859d89759

net/mlx5: fix sft PMD
Update PMD internal sft rules creation scheme.

Fixes: 9717598ca846 ("net/mlx5: add sft pmd implementation")

Signed-off-by: Gregory Etelson <getelson@nvidia.com>

drivers/net/mlx5/mlx5_flow_dv.c
@@ -10170,7 +10170,7 @@ flow_dv_sft_create_cb(struct mlx5_hlist *list, uint64_t key64, void *cb_ctx)
 	/* Create an SFT flow */
 	sft_data->sft_flow_idx =
 		mlx5_flow_add_post_sft_rule(dev,
-					    MLX5_FLOW_TABLE_LEVEL_POST_SFT,
+					    MLX5_FLOW_TABLE_SFT_L2,
 					    reg, sft_data->jump_group,
 					    0, error);
 	if (!sft_data->sft_flow_idx)
@@ -12662,7 +12662,7 @@ flow_dv_translate(struct rte_eth_dev *dev,
 			jump_group = ((const struct rte_flow_action_jump *)
 							action->conf)->group;
 			if (action_flags & MLX5_FLOW_ACTION_SFT)
-				jump_group = MLX5_FLOW_TABLE_LEVEL_SFT;
+				jump_group = MLX5_FLOW_TABLE_SFT_L0;
 			grp_info.std_tbl_fix = 0;
 			if (dev_flow->skip_scale &
 				(1 << MLX5_SCALE_JUMP_FLOW_GROUP_BIT))

drivers/net/mlx5/linux/mlx5_os.c
@@ -2432,7 +2432,7 @@ mlx5_os_pci_probe_pf(struct rte_pci_device *pci_dev,
 		dev_config.dv_flow_en = 1;
 		dev_config.dv_validate_mod = 0;
 		dev_config.decap_en = 1;
-		dev_config.sft_en = 0;
+		dev_config.sft_en = 1;
 		dev_config.log_hp_size = MLX5_ARG_UNSET;
 		dev_config.log_max_mtr_num = 0xFF;
 		dev_config.log_max_flow_per_mtr = 0xFF;

drivers/net/mlx5/mlx5.h
@@ -872,10 +872,10 @@ struct mlx5_flow_tbl_resource {
 #define MLX5_FLOW_TABLE_LEVEL_SUFFIX (MLX5_MAX_TABLES - 3)
 #define MLX5_FLOW_TABLE_LEVEL_METER (MLX5_MAX_TABLES - 4)
 /* SFT tables */
-#define MLX5_FLOW_TABLE_LEVEL_SFT (MLX5_MAX_TABLES - 5)
-#define MLX5_FLOW_TABLE_LEVEL_ITMD_SFT (MLX5_MAX_TABLES - 6)
-#define MLX5_FLOW_TABLE_LEVEL_POST_SFT (MLX5_MAX_TABLES - 7)
-#define MLX5_MAX_TABLES_EXTERNAL MLX5_FLOW_TABLE_LEVEL_POST_SFT
+#define MLX5_FLOW_TABLE_SFT_L0 (MLX5_MAX_TABLES - 7)
+#define MLX5_FLOW_TABLE_SFT_L1 (MLX5_MAX_TABLES - 6)
+#define MLX5_FLOW_TABLE_SFT_L2 (MLX5_MAX_TABLES - 5)
+#define MLX5_MAX_TABLES_EXTERNAL MLX5_FLOW_TABLE_SFT_L0
 #define MLX5_MAX_TABLES_FDB UINT16_MAX
 #define MLX5_FLOW_TABLE_FACTOR 10
 
@@ -1276,6 +1276,21 @@ struct mlx5_obj_ops {
 
 #define MLX5_RSS_HASH_FIELDS_LEN RTE_DIM(mlx5_rss_hash_fields)
 
+enum {
+	MLX5_SFT_L0_DFLT_ZONE_FLOW = 0,
+	MLX5_SFT_L0_DFLT_IPV4_FRAG_FLOW,
+	MLX5_SFT_L0_DFLT_IPV6_FRAG_FLOW,
+	MLX5_SFT_L0_DFLT_FLOWS_NUM /* keep this item the last */
+};
+
+/*
+ * default SFT flows
+ */
+struct sft_flows {
+	struct rte_flow *l0_flows[MLX5_SFT_L0_DFLT_FLOWS_NUM];
+	struct rte_flow *l1_flow;
+};
+
 struct mlx5_priv {
 	struct rte_eth_dev_data *dev_data;  /* Pointer to device data. */
 	struct mlx5_dev_ctx_shared *sh; /* Shared device context. */
@@ -1360,6 +1375,7 @@ struct mlx5_priv {
 	uint32_t *sft_lists;
 	struct mlx5_devx_obj *q_counters; /* DevX queue counter object. */
 	uint32_t counter_set_id; /* Queue counter ID to set in DevX objects. */
+	struct sft_flows *sft_flows;
 };
 
 #define PORT_ID(priv) ((priv)->dev_data->port_id)

drivers/net/mlx5/mlx5_flow.c
@@ -754,8 +754,6 @@ mlx5_flow_get_reg_id(struct rte_eth_dev *dev,
 		return REG_B;
 	case MLX5_HAIRPIN_TX:
 		return REG_A;
-	case MLX5_SFT_FID:
-	case MLX5_SFT_ZONE:
 	case MLX5_METADATA_RX:
 		switch (config->dv_xmeta_en) {
 		case MLX5_XMETA_MODE_LEGACY:
@@ -778,7 +776,6 @@ mlx5_flow_get_reg_id(struct rte_eth_dev *dev,
 			return REG_C_1;
 		}
 		break;
-	case MLX5_SFT_APP_STATE:
 	case MLX5_FLOW_MARK:
 		switch (config->dv_xmeta_en) {
 		case MLX5_XMETA_MODE_LEGACY:
@@ -870,6 +867,11 @@ mlx5_flow_get_reg_id(struct rte_eth_dev *dev,
 		return config->flow_mreg_c[id + start_reg - REG_C_0];
 	case MLX5_SFT_APP_DATA:
 		return REG_C_0;
+	case MLX5_SFT_APP_STATE:
+		return REG_C_1;
+	case MLX5_SFT_FID:
+	case MLX5_SFT_ZONE:
+		return REG_C_5;
 	case MLX5_SFT_JUMP_GROUP:
 		return REG_C_4;
 	}

drivers/net/mlx5/mlx5_flow.h
@@ -96,8 +96,10 @@ union sft_mark {
 	struct {
 		uint32_t fid_valid:1;
 		uint32_t zone_valid:1;
+		uint32_t direction:1;
+		uint32_t fragment:1;
 		uint32_t control:1;
-		uint32_t reserved:13;
+		uint32_t reserved:11;
 		uint32_t app_state:8;
 		uint32_t _unused_:8;
 	};
@@ -117,12 +119,12 @@ enum mlx5_feature_name {
 	MLX5_MTR_ID,
 	MLX5_MTR_FLOW_ID,
 	MLX5_ASO_FLOW_HIT,
+	MLX5_ASO_CONNTRACK,
 	MLX5_SFT_FID,
 	MLX5_SFT_ZONE,
 	MLX5_SFT_APP_DATA,
 	MLX5_SFT_APP_STATE,
 	MLX5_SFT_JUMP_GROUP,
-	MLX5_ASO_CONNTRACK,
 };
 
 /* Default queue number. */
@@ -623,7 +625,8 @@ struct mlx5_flow_tbl_data_entry {
 /* SFT data structure of the hash organization. */
 struct mlx5_flow_sft_data_entry {
 	struct mlx5_hlist_entry entry; /**< hash list entry. */
-	uint32_t jump_group; /**< Resource key. */
+	uint32_t jump_group; /**< SFT app group part of match key. */
+	uint32_t port_id; /**< port id. part of match key */
 	uint32_t sft_flow_idx; /**< SFT flow index. */
 	uint32_t idx; /**< Index in mempool. */
 };
@@ -1573,9 +1576,10 @@ int flow_dv_sft_match_cb(struct mlx5_hlist *list,
 			 void *cb_ctx);
 void flow_dv_sft_remove_cb(struct mlx5_hlist *list,
 			   struct mlx5_hlist_entry *entry);
-struct mlx5_flow_sft_data_entry *flow_dv_sft_resource_get(struct rte_eth_dev *dev,
-		uint32_t jump_group, struct rte_flow_error *error);
-
+struct mlx5_flow_sft_data_entry *
+flow_dv_sft_resource_get(struct rte_eth_dev *dev,
+			 uint32_t jump_group,
+			 struct rte_flow_error *error);
 struct mlx5_hlist_entry *flow_dv_tag_create_cb(struct mlx5_hlist *list,
 					       uint64_t key, void *cb_ctx);
 int flow_dv_tag_match_cb(struct mlx5_hlist *list,

drivers/net/mlx5/mlx5_sft.h
@@ -5,14 +5,32 @@
 #ifndef RTE_PMD_MLX5_MLX5_H
 #define RTE_PMD_MLX5_MLX5_H
 
+#include <rte_flow.h>
+#include <rte_sft_driver.h>
+
+#define MLX5_SFT_L1_IMPLICIT_ACTIONS_NUM 4
+#define MLX5_SFT_L1_ACTIONS_NUM (SFT_ACTIONS_NUM + \
+				 MLX5_SFT_L1_IMPLICIT_ACTIONS_NUM)
+__extension__
+union mlx5_sft_entry_flags {
+	uint8_t val;
+	struct {
+		uint8_t initiator:1;
+		uint8_t ipv4:1;
+		uint8_t ipv6:1;
+		uint8_t udp:1;
+		uint8_t tcp:1;
+	};
+};
+
 struct rte_sft_entry {
 	ILIST_ENTRY(uint32_t)next;
-	struct rte_flow *flow;
-	struct rte_flow *itmd_flow;
-	struct rte_flow *miss_flow;
 	uint32_t idx;
-	uint32_t state;
-	uint32_t fid_zone;
+	uint32_t fid;
+	union mlx5_sft_entry_flags flags;
+	uint64_t miss_conditions;
+	struct rte_flow *sft_l0_flow;
+	struct rte_flow *sft_l1_flow;
 };
 
 #define MLX5_SFT_QUEUE_MAX			(64)

commit f42b6a7cf086223ddf56e09fe86ffb331c61e8ac

net/mlx5: fix SFT item match
Fix state register assignment in table L0.

Fixes: 9af445c6d6c0 ("net/mlx5: fix sft PMD")
Signed-off-by: Gregory Etelson <getelson@nvidia.com>

drivers/net/mlx5/mlx5_flow_dv.c
@@ -10171,7 +10171,8 @@ flow_dv_translate_item_sft(struct rte_eth_dev *dev, void *match_mask,
 		 * for matching.
 		 */
 		flow_dv_translate_sft_item_data(dev, app_state_reg,
-						mark_value.val, mark_mask.val,
+						mark_value.val & mark_mask.val,
+						mark_mask.val,
 						match_mask, match_value);
 	}
 	/* PMD can match FID or zone

drivers/net/mlx5/mlx5_sft.c
@@ -657,6 +657,11 @@ mlx5_sft_add_l1_rules(struct rte_eth_dev *dev, struct rte_sft_entry *entry,
 		.ingress = 1,
 		.group = MLX5_FLOW_TABLE_SFT_L1,
 	};
+	const union sft_mark mark = {
+		.fid_valid = 1,
+		.zone_valid = 1,
+		.app_state = state,
+	};
 	int fid_reg = mlx5_flow_get_reg_id(dev, MLX5_SFT_FID, 0, &rte_err);
 	int state_reg = mlx5_flow_get_reg_id(dev, MLX5_SFT_APP_STATE, 0,
 					     &rte_err);
@@ -711,9 +716,9 @@ mlx5_sft_add_l1_rules(struct rte_eth_dev *dev, struct rte_sft_entry *entry,
 				MLX5_RTE_FLOW_ACTION_TYPE_TAG,
 			.conf = &(struct mlx5_rte_flow_action_set_tag){
 				.id = (enum modify_reg)state_reg,
-				.data = state,
-				.length = 8,
-				.offset = 16
+				.data = mark.val,
+				.length = MLX5_REG_BITS,
+				.offset = 0
 			}
 		},
 		[2] = {
@@ -766,7 +771,7 @@ mlx5_sft_add_l0_rules(struct sft_entry_ctx *ctx, struct rte_sft_entry *entry,
 {
 	int zone_fid_reg, state_reg, i = ctx->hit_actions_num;
 	struct rte_flow_error rte_err;
-	const union sft_mark mark = {
+	union sft_mark mark = {
 		.fid_valid = 1,
 		.direction = ctx->flags.initiator
 	};
@@ -815,6 +820,7 @@ mlx5_sft_add_l0_rules(struct sft_entry_ctx *ctx, struct rte_sft_entry *entry,
 			.mask = UINT32_MAX
 		}
 	};
+	mark.zone_valid = 1;
 	ctx->l0_actions[i++] = (typeof(ctx->l0_actions[0])) {
 		.type = (enum rte_flow_action_type)
 			MLX5_RTE_FLOW_ACTION_TYPE_TAG,