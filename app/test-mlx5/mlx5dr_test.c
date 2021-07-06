b5e15e6caa5c9f1336160c29bb3219ececd924d6
Alex Vesker <valex@nvidia.com>
2021-07-06 15:01:01 +0300

    mlx5dr: Enhannce test
    Should be run with:
    dpdk-test-mlx5 --no-huge
    
    Signed-off-by: Alex Vesker <valex@nvidia.com>

--- app/test-mlx5/mlx5dr_test.c
+++ app/test-mlx5/mlx5dr_test.c
@@ -15,21 +15,76 @@ char dev_name[] = "mlx5_0";
 static int run_test(struct ibv_context *ibv_ctx)
 {
 	struct mlx5dr_context *ctx;
+	struct mlx5dr_table *root_tbl, *hws_tbl;
+	struct mlx5dr_matcher *root_matcher, *hws_matcher;
 	struct mlx5dr_context_attr dr_ctx_attr = {0};
+	struct mlx5dr_table_attr dr_tbl_attr = {0};
+	struct mlx5dr_matcher_attr matcher_attr = {0};
 
 	dr_ctx_attr.initial_log_ste_memory = 0;
 	dr_ctx_attr.pd = NULL;
 	dr_ctx_attr.queues = 16;
 	dr_ctx_attr.queues_size = 1024;
-
 	ctx = mlx5dr_context_open(ibv_ctx, &dr_ctx_attr);
-	if (!ctx)
+	if (!ctx) {
+		printf("Failed to create context\n");
 		goto out_err;
+	}
 
+	/* Create root table */
+	dr_tbl_attr.level = 0;
+	dr_tbl_attr.type = MLX5DR_TABLE_TYPE_NIC_RX;
+	root_tbl = mlx5dr_table_create(ctx, &dr_tbl_attr);
+	if (!root_tbl) {
+		printf("Failed to create root table\n");
+		goto close_ctx;
+	}
+
+	/* Create HWS table */
+	dr_tbl_attr.level = 1;
+	dr_tbl_attr.type = MLX5DR_TABLE_TYPE_NIC_RX;
+	hws_tbl = mlx5dr_table_create(ctx, &dr_tbl_attr);
+	if (!hws_tbl) {
+		printf("Failed to create HWS table\n");
+		goto destroy_root_tbl;
+	}
+
+	/* Create root matcher */
+	matcher_attr.priority = 0;
+	matcher_attr.insertion_mode = MLX5DR_MATCHER_INSERTION_MODE_ASSURED;
+	root_matcher = mlx5dr_matcher_create(root_tbl, NULL, &matcher_attr);
+	if (!root_matcher) {
+		printf("Failed to create root matcher\n");
+		goto destroy_hws_tbl;
+	}
+
+	/* Create HWS matcher */
+	matcher_attr.priority = 0;
+	matcher_attr.insertion_mode = MLX5DR_MATCHER_INSERTION_MODE_BEST_EFFORT;
+	matcher_attr.size_hint_column_log = 4;
+	matcher_attr.size_hint_rows_log = 4;
+	hws_matcher = mlx5dr_matcher_create(hws_tbl, NULL, &matcher_attr);
+	if (!hws_matcher) {
+		printf("Failed to create HWS matcher\n");
+		goto destroy_root_matcher;
+	}
+
+	mlx5dr_matcher_destroy(hws_matcher);
+	mlx5dr_matcher_destroy(root_matcher);
+	mlx5dr_table_destroy(hws_tbl);
+	mlx5dr_table_destroy(root_tbl);
 	mlx5dr_context_close(ctx);
 
 	return 0;
 
+destroy_root_matcher:
+	mlx5dr_matcher_destroy(root_matcher);
+destroy_hws_tbl:
+	mlx5dr_table_destroy(hws_tbl);
+destroy_root_tbl:
+	mlx5dr_table_destroy(root_tbl);
+close_ctx:
+	mlx5dr_context_close(ctx);
 out_err:
 	return -1;
 }
@@ -42,12 +97,13 @@ int main(int argc, char **argv)
 	struct ibv_device *dev;
 	int i, ret;
 
-	if (argc && argv)
-		printf("ignoring params");
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		rte_exit(EXIT_FAILURE, ":: invalid EAL arguments\n");
 
 	dev_list = ibv_get_device_list(NULL);
 	if (!dev_list) {
-		printf("Failed to get IB devices list");
+		printf("Failed to get IB devices list\n");
 		return 1;
 	}
 
