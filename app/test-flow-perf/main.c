LiZhang-2020/dpdk

commit 490d5f4cfaa5b5ffb879f220c406e52678af0c2c

ethdev: add meter PPS profile
Currently meter algorithms only supports rate is bytes per second(BPS).
Add this new meter srTCMp algorithm to support rate is packet per second.
So that it can meter traffic by packet per second.
The below structure will be extended:
rte_mtr_algorithm
rte_mtr_meter_profile

Signed-off-by: Li Zhang <lizh@nvidia.com>

lib/librte_ethdev/rte_mtr.h
@@ -119,6 +119,11 @@ enum rte_mtr_algorithm {
 
 	/** Two Rate Three Color Marker (trTCM) - IETF RFC 4115. */
 	RTE_MTR_TRTCM_RFC4115,
+
+	/** Single Rate Three Color Marker, Packet based (srTCMp).
+	 * - - similar to IETF RFC 2697 but rate is packet per second.
+	 */
+	RTE_MTR_SRTCMP,
 };
 
 /**
@@ -171,6 +176,20 @@ struct rte_mtr_meter_profile {
 			/** Excess Burst Size (EBS) (bytes). */
 			uint64_t ebs;
 		} trtcm_rfc4115;
+
+		/** Items only valid when *alg* is set to srTCMp. */
+		struct {
+			/** Committed Information Rate (CIR)
+			 * (packets/second).
+			 */
+			uint64_t cir;
+
+			/** Committed Burst Size (CBS) (packets). */
+			uint64_t cbs;
+
+			/** Excess Burst Size (EBS) (packets). */
+			uint64_t ebs;
+		} srtcmp;
 	};
 };
 
@@ -317,6 +336,13 @@ struct rte_mtr_capabilities {
 	 */
 	uint32_t meter_trtcm_rfc4115_n_max;
 
+	/** Maximum number of MTR objects that can have their meter configured
+	 * to run the srTCMp algorithm. The value of 0
+	 * indicates this metering algorithm is not supported.
+	 * The maximum value is *n_max*.
+	 */
+	uint32_t meter_srtcmp_n_max;
+
 	/** Maximum traffic rate that can be metered by a single MTR object. For
 	 * srTCM RFC 2697, this is the maximum CIR rate. For trTCM RFC 2698,
 	 * this is the maximum PIR rate. For trTCM RFC 4115, this is the maximum
@@ -341,6 +367,12 @@ struct rte_mtr_capabilities {
 	 * the trTCM RFC 4115 metering algorithm.
 	 */
 	int color_aware_trtcm_rfc4115_supported;
+
+	/**
+	 * When non-zero, it indicates that color aware mode is supported for
+	 * the srTCMp metering algorithm.
+	 */
+	int color_aware_srtcmp_supported;
 
 	/** When non-zero, it indicates that the policer packet recolor actions
 	 * are supported.

commit 4f5888fca5e9231c59088bfbe125331a4d14082e

ethdev: fix meter PPS profile old solution
Code clean up the old solution (srTCMp) to
support meter packet mode.

Fixes: 490d5f4cfaa ("ethdev: add meter PPS profile")
Cc: stable@dpdk.org

Signed-off-by: Li Zhang <lizh@nvidia.com>

app/test-flow-perf/main.c
@@ -44,7 +44,6 @@
 #define DEFAULT_RULES_BATCH     100000
 #define DEFAULT_GROUP                0
 #define METER_PROFILE_ALG_SRTCM_RFC2697    "srtcm_rfc2697"
-#define METER_PROFILE_ALG_SRTCMP           "srtcmp"
 #define DEFAULT_METER_PROFILE_ALG          METER_PROFILE_ALG_SRTCM_RFC2697
 
 struct rte_flow *flow;
@@ -72,7 +71,6 @@ static uint32_t rules_count;
 static uint32_t rules_batch;
 static uint32_t hairpin_queues_num; /* total hairpin q number - default: 0 */
 static uint32_t nb_lcores;
-static uint32_t mtr_profile_alg;
 
 #define MAX_PKT_BURST    32
 #define LCORE_MODE_PKT    1
@@ -136,8 +134,6 @@ usage(char *progname)
 	printf("  --enable-fwd: To enable packets forwarding"
 		" after insertion\n");
 	printf("  --portmask=N: hexadecimal bitmask of ports used\n");
-	printf("  --meter-profile-alg=str: to set the traffic metering"
-		" algorithm, default is %s\n", DEFAULT_METER_PROFILE_ALG);
 	printf("  --unique-data: flag to set using unique data for all"
 		" actions that support data, such as header modify and encap actions\n");
 
@@ -579,7 +575,6 @@ args_parse(int argc, char **argv)
 		{ "unique-data",                0, 0, 0 },
 		{ "portmask",                   1, 0, 0 },
 		{ "cores",                      1, 0, 0 },
-		{ "meter-profile-alg",          1, 0, 0 },
 		/* Attributes */
 		{ "ingress",                    0, 0, 0 },
 		{ "egress",                     0, 0, 0 },
@@ -808,17 +803,6 @@ args_parse(int argc, char **argv)
 						"Error: cores count must be > 0 and < %d\n",
 						RTE_MAX_LCORE);
 				}
-			}
-			if (strcmp(lgopts[opt_idx].name,
-					"meter-profile-alg") == 0) {
-				if (strcmp(optarg, "srtcm_rfc2697") == 0)
-					mtr_profile_alg = RTE_MTR_SRTCM_RFC2697;
-				else if (strcmp(optarg, "srtcmp") == 0)
-					mtr_profile_alg = RTE_MTR_SRTCMP;
-				else
-					rte_exit(EXIT_FAILURE, "\nError: not supported meter profile algorithm %s\n"
-						 "Current supported algorithms are: [%s, %s]\n",
-						 optarg, METER_PROFILE_ALG_SRTCM_RFC2697, METER_PROFILE_ALG_SRTCMP);
 			}
 			break;
 		default:
@@ -1069,17 +1053,11 @@ create_meter_profile(void)
 		/* If port outside portmask */
 		if (!((ports_mask >> port_id) & 0x1))
 			continue;
-		if (mtr_profile_alg == RTE_MTR_SRTCMP) {
-			mp.alg = RTE_MTR_SRTCMP;
-			mp.srtcmp.cir = METER_CIR;
-			mp.srtcmp.cbs = METER_CIR / 8;
-			mp.srtcmp.ebs = 0;
-		} else {
-			mp.alg = RTE_MTR_SRTCM_RFC2697;
-			mp.srtcm_rfc2697.cir = METER_CIR;
-			mp.srtcm_rfc2697.cbs = METER_CIR / 8;
-			mp.srtcm_rfc2697.ebs = 0;
-		}
+		mp.alg = RTE_MTR_SRTCM_RFC2697;
+		mp.srtcm_rfc2697.cir = METER_CIR;
+		mp.srtcm_rfc2697.cbs = METER_CIR / 8;
+		mp.srtcm_rfc2697.ebs = 0;
+
 		ret = rte_mtr_meter_profile_add
 			(port_id, DEFAULT_METER_PROF_ID, &mp, &error);
 		if (ret != 0) {

app/test-pmd/cmdline.c
@@ -698,9 +698,6 @@ static void cmd_help_long_parsed(void *parsed_result,
 			"add port meter profile trtcm_rfc4115 (port_id) (profile_id) (cir) (eir) (cbs) (ebs) (packet_mode)\n"
 			"    meter profile add - trtcm rfc 4115\n\n"
 
-			"add port meter profile srtcmp (port_id) (profile_id) (cir) (cbs) (ebs)\n"
-			"    meter profile add - srtcmp packet per second\n\n"
-
 			"del port meter profile (port_id) (profile_id)\n"
 			"    meter profile delete\n\n"
 
@@ -16982,7 +16979,6 @@ cmdline_parse_ctx_t main_ctx[] = {
 	(cmdline_parse_inst_t *)&cmd_show_port_meter_cap,
 	(cmdline_parse_inst_t *)&cmd_add_port_meter_profile_srtcm,
 	(cmdline_parse_inst_t *)&cmd_add_port_meter_profile_trtcm,
-	(cmdline_parse_inst_t *)&cmd_add_port_meter_profile_srtcmp,
 	(cmdline_parse_inst_t *)&cmd_del_port_meter_profile,
 	(cmdline_parse_inst_t *)&cmd_create_port_meter,
 	(cmdline_parse_inst_t *)&cmd_enable_port_meter,

app/test-pmd/cmdline_mtr.c
@@ -256,8 +256,6 @@ static void cmd_show_port_meter_cap_parsed(void *parsed_result,
 		cap.meter_trtcm_rfc2698_n_max);
 	printf("cap.meter_trtcm_rfc4115_n_max %" PRIu32 "\n",
 		cap.meter_trtcm_rfc4115_n_max);
-	printf("cap.meter_srtcmp_n_max %" PRIu32 "\n",
-		cap.meter_srtcmp_n_max);
 	printf("cap.meter_rate_max %" PRIu64 "\n", cap.meter_rate_max);
 	printf("cap.color_aware_srtcm_rfc2697_supported %" PRId32 "\n",
 		cap.color_aware_srtcm_rfc2697_supported);
@@ -633,107 +631,6 @@ cmdline_parse_inst_t cmd_add_port_meter_profile_trtcm_rfc4115 = {
 		(void *)&cmd_add_port_meter_profile_trtcm_rfc4115_cbs,
 		(void *)&cmd_add_port_meter_profile_trtcm_rfc4115_ebs,
 		(void *)&cmd_add_port_meter_profile_trtcm_rfc4115_packet_mode,
-		NULL,
-	},
-};
-
-/* *** Add Port Meter Profile srtcmp *** */
-struct cmd_add_port_meter_profile_srtcmp_result {
-	cmdline_fixed_string_t add;
-	cmdline_fixed_string_t port;
-	cmdline_fixed_string_t meter;
-	cmdline_fixed_string_t profile;
-	cmdline_fixed_string_t srtcmp;
-	uint16_t port_id;
-	uint32_t profile_id;
-	uint64_t cir;
-	uint64_t cbs;
-	uint64_t ebs;
-};
-
-cmdline_parse_token_string_t cmd_add_port_meter_profile_srtcmp_add =
-	TOKEN_STRING_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result, add, "add");
-cmdline_parse_token_string_t cmd_add_port_meter_profile_srtcmp_port =
-	TOKEN_STRING_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			port, "port");
-cmdline_parse_token_string_t cmd_add_port_meter_profile_srtcmp_meter =
-	TOKEN_STRING_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			meter, "meter");
-cmdline_parse_token_string_t cmd_add_port_meter_profile_srtcmp_profile =
-	TOKEN_STRING_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			profile, "profile");
-cmdline_parse_token_string_t cmd_add_port_meter_profile_srtcmp_srtcmp =
-	TOKEN_STRING_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			srtcmp, "srtcmp");
-cmdline_parse_token_num_t cmd_add_port_meter_profile_srtcmp_port_id =
-	TOKEN_NUM_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			port_id, RTE_UINT16);
-cmdline_parse_token_num_t cmd_add_port_meter_profile_srtcmp_profile_id =
-	TOKEN_NUM_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			profile_id, RTE_UINT32);
-cmdline_parse_token_num_t cmd_add_port_meter_profile_srtcmp_cir =
-	TOKEN_NUM_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			cir, RTE_UINT64);
-cmdline_parse_token_num_t cmd_add_port_meter_profile_srtcmp_cbs =
-	TOKEN_NUM_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			cbs, RTE_UINT64);
-cmdline_parse_token_num_t cmd_add_port_meter_profile_srtcmp_ebs =
-	TOKEN_NUM_INITIALIZER(
-		struct cmd_add_port_meter_profile_srtcmp_result,
-			ebs, RTE_UINT64);
-
-static void cmd_add_port_meter_profile_srtcmp_parsed(void *parsed_result,
-	__rte_unused struct cmdline *cl,
-	__rte_unused void *data)
-{
-	struct cmd_add_port_meter_profile_srtcmp_result *res = parsed_result;
-	struct rte_mtr_meter_profile mp;
-	struct rte_mtr_error error;
-	uint32_t profile_id = res->profile_id;
-	uint16_t port_id = res->port_id;
-	int ret;
-
-	if (port_id_is_invalid(port_id, ENABLED_WARN))
-		return;
-
-	/* Private shaper profile params */
-	memset(&mp, 0, sizeof(struct rte_mtr_meter_profile));
-	mp.alg = RTE_MTR_SRTCMP;
-	mp.srtcmp.cir = res->cir;
-	mp.srtcmp.cbs = res->cbs;
-	mp.srtcmp.ebs = res->ebs;
-
-	ret = rte_mtr_meter_profile_add(port_id, profile_id, &mp, &error);
-	if (ret != 0) {
-		print_err_msg(&error);
-		return;
-	}
-}
-
-cmdline_parse_inst_t cmd_add_port_meter_profile_srtcmp = {
-	.f = cmd_add_port_meter_profile_srtcmp_parsed,
-	.data = NULL,
-	.help_str = "add port meter profile srtcmp <port_id> <profile_id> <cir> <cbs> <ebs>",
-	.tokens = {
-		(void *)&cmd_add_port_meter_profile_srtcmp_add,
-		(void *)&cmd_add_port_meter_profile_srtcmp_port,
-		(void *)&cmd_add_port_meter_profile_srtcmp_meter,
-		(void *)&cmd_add_port_meter_profile_srtcmp_profile,
-		(void *)&cmd_add_port_meter_profile_srtcmp_srtcmp,
-		(void *)&cmd_add_port_meter_profile_srtcmp_port_id,
-		(void *)&cmd_add_port_meter_profile_srtcmp_profile_id,
-		(void *)&cmd_add_port_meter_profile_srtcmp_cir,
-		(void *)&cmd_add_port_meter_profile_srtcmp_cbs,
-		(void *)&cmd_add_port_meter_profile_srtcmp_ebs,
 		NULL,
 	},
 };

app/test-pmd/cmdline_mtr.h
@@ -11,7 +11,6 @@ extern cmdline_parse_inst_t cmd_show_port_meter_cap;
 extern cmdline_parse_inst_t cmd_add_port_meter_profile_srtcm;
 extern cmdline_parse_inst_t cmd_add_port_meter_profile_trtcm;
 extern cmdline_parse_inst_t cmd_add_port_meter_profile_trtcm_rfc4115;
-extern cmdline_parse_inst_t cmd_add_port_meter_profile_srtcmp;
 extern cmdline_parse_inst_t cmd_del_port_meter_profile;
 extern cmdline_parse_inst_t cmd_create_port_meter;
 extern cmdline_parse_inst_t cmd_enable_port_meter;

lib/librte_ethdev/rte_mtr.h
@@ -120,11 +120,6 @@ enum rte_mtr_algorithm {
 
 	/** Two Rate Three Color Marker (trTCM) - IETF RFC 4115. */
 	RTE_MTR_TRTCM_RFC4115,
-
-	/** Single Rate Three Color Marker, Packet based (srTCMp).
-	 * - - similar to IETF RFC 2697 but rate is packet per second.
-	 */
-	RTE_MTR_SRTCMP,
 };
 
 /**
@@ -192,20 +187,6 @@ struct rte_mtr_meter_profile {
 			/** Excess Burst Size (EBS) (bytes or packets). */
 			uint64_t ebs;
 		} trtcm_rfc4115;
-
-		/** Items only valid when *alg* is set to srTCMp. */
-		struct {
-			/** Committed Information Rate (CIR)
-			 * (packets/second).
-			 */
-			uint64_t cir;
-
-			/** Committed Burst Size (CBS) (packets). */
-			uint64_t cbs;
-
-			/** Excess Burst Size (EBS) (packets). */
-			uint64_t ebs;
-		} srtcmp;
 	};
 
 	/**
@@ -357,13 +338,6 @@ struct rte_mtr_capabilities {
 	 * metering algorithm is not supported. The maximum value is *n_max*.
 	 */
 	uint32_t meter_trtcm_rfc4115_n_max;
-
-	/** Maximum number of MTR objects that can have their meter configured
-	 * to run the srTCMp algorithm. The value of 0
-	 * indicates this metering algorithm is not supported.
-	 * The maximum value is *n_max*.
-	 */
-	uint32_t meter_srtcmp_n_max;
 
 	/** Maximum traffic rate that can be metered by a single MTR object. For
 	 * srTCM RFC 2697, this is the maximum CIR rate. For trTCM RFC 2698,