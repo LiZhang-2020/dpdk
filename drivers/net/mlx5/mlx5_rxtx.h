60394957cc675b3d1539f38bda8ac8c1657a2fa5
Michael Baum <michaelba@nvidia.com>
2021-10-19 23:55:56 +0300

    net/mlx5: remove redundancy in MR file
    This patch remove two redundant things from MR file:
    
    1. mr_find_contig_memsegs_data structure which is moved to common file
       before.
    2. External memory mechanism - mlx5_tx_update_ext_mp function.
       Since commit [1] which added support for DMA map and unmap, external
       mem must be configured by the user using rte_mem_map function and no
       need to handle this in pmd.
    
    [1]
    commit 989e999d9305
    ("net/mlx5: support PCI device DMA map and unmap")
    
    Signed-off-by: Michael Baum <michaelba@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>

--- drivers/net/mlx5/mlx5_rxtx.h
+++ drivers/net/mlx5/mlx5_rxtx.h
@@ -492,8 +492,6 @@ uint16_t mlx5_rx_burst_mprq_vec(void *dpdk_txq, struct rte_mbuf **pkts,
 /* mlx5_mr.c */
 
 uint32_t mlx5_tx_mb2mr_bh(struct mlx5_txq_data *txq, struct rte_mbuf *mb);
-uint32_t mlx5_tx_update_ext_mp(struct mlx5_txq_data *txq, uintptr_t addr,
-			       struct rte_mempool *mp);
 int mlx5_net_dma_map(struct rte_device *rte_dev, void *addr, uint64_t iova,
 		     size_t len);
 int mlx5_net_dma_unmap(struct rte_device *rte_dev, void *addr, uint64_t iova,
