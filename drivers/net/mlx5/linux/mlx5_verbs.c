LiZhang-2020/dpdk

commit c34e422b683013d81d63a1ae7279f3714573e001

net/mlx5: support receive memory pool
This patch creates DevX RMP and RMP based RQ for shared Rx queue.

Signed-off-by: Xueming Li <xuemingl@nvidia.com>

drivers/net/mlx5/mlx5.h
@@ -1279,6 +1279,7 @@ struct mlx5_rxq_obj {
 			void *ibv_channel;
 		};
 		struct {
+			struct mlx5_devx_obj *devx_rmp; /* DevX RMP object. */
 			struct mlx5_devx_obj *devx_cq; /* DevX CQ object. */
 			void *devx_channel;
 		};

drivers/net/mlx5/mlx5_devx.c
@@ -316,16 +316,70 @@ static void
 mlx5_devx_wq_attr_fill(struct mlx5_priv *priv, struct mlx5_rxq_ctrl *rxq_ctrl,
 		       struct mlx5_devx_wq_attr *wq_attr)
 {
+	struct mlx5_rxq_data *rxq_data = &rxq_ctrl->rxq;
+	uint32_t wqe_size = 0;
+	uint32_t log_wqe_size = 0;
+
 	wq_attr->end_padding_mode = priv->config.hw_padding ?
 					MLX5_WQ_END_PAD_MODE_ALIGN :
 					MLX5_WQ_END_PAD_MODE_NONE;
-	wq_attr->pd = priv->sh->pdn;
+	wq_attr->pd = rxq_ctrl->sh->pdn;
+	if (mlx5_rxq_mprq_enabled(rxq_data)) {
+		wq_attr->wq_type = MLX5_WQ_TYPE_CYCLIC_STRIDING_RQ;
+		/*
+		 * Number of strides in each WQE:
+		 * 512*2^single_wqe_log_num_of_strides.
+		 */
+		wq_attr->single_wqe_log_num_of_strides =
+				rxq_data->strd_num_n -
+				MLX5_MIN_SINGLE_WQE_LOG_NUM_STRIDES;
+		/* Stride size = (2^single_stride_log_num_of_bytes)*64B. */
+		wq_attr->single_stride_log_num_of_bytes =
+				rxq_data->strd_sz_n -
+				MLX5_MIN_SINGLE_STRIDE_LOG_NUM_BYTES;
+		wqe_size = sizeof(struct mlx5_wqe_mprq);
+	} else {
+		wq_attr->wq_type = MLX5_WQ_TYPE_CYCLIC;
+		wqe_size = sizeof(struct mlx5_wqe_data_seg);
+	}
+	log_wqe_size = log2above(wqe_size) + rxq_data->sges_n;
+	wq_attr->log_wq_stride = log_wqe_size;
+	wq_attr->log_wq_sz = rxq_data->elts_n - rxq_data->sges_n;
 	wq_attr->dbr_addr = rxq_ctrl->rq_dbr_offset;
 	wq_attr->dbr_umem_id =
 			mlx5_os_get_umem_id(rxq_ctrl->rq_dbrec_page->umem);
 	wq_attr->dbr_umem_valid = 1;
 	wq_attr->wq_umem_id = mlx5_os_get_umem_id(rxq_ctrl->wq_umem);
 	wq_attr->wq_umem_valid = 1;
+}
+
+/**
+ * Initialize a receive mempool object using DevX.
+ *
+ * @param rxq
+ *   Pointer to Rx queue.
+ *
+ * @return
+ *   The DevX RMP object initialized, NULL otherwise and rte_errno is set.
+ */
+static struct mlx5_devx_obj *
+mlx5_rxq_create_devx_rmp(struct mlx5_rxq_priv *rxq)
+{
+	struct mlx5_priv *priv = rxq->priv;
+	struct mlx5_rxq_ctrl *rxq_ctrl = rxq->ctrl;
+	struct mlx5_devx_create_rmp_attr rmp_attr = { 0 };
+	struct mlx5_devx_obj *rmp;
+
+	/* Fill RMP attributes. */
+	rmp_attr.state = MLX5_RMPC_STATE_RDY;
+	rmp_attr.basic_cyclic_rcv_wqe = mlx5_rxq_mprq_enabled(&rxq_ctrl->rxq) ?
+					0 : 1;
+	/* Fill WQ attributes. */
+	mlx5_devx_wq_attr_fill(priv, rxq_ctrl, &rmp_attr.wq_attr);
+	/* Create RMP using DevX API. */
+	rmp = mlx5_devx_cmd_create_rmp(priv->sh->ctx, &rmp_attr,
+				       rxq_ctrl->socket);
+	return rmp;
 }
 
 /**
@@ -414,46 +468,20 @@ mlx5_rxq_create_devx_rq(struct mlx5_rxq_priv *rxq)
 	struct mlx5_rxq_data *rxq_data = &rxq->ctrl->rxq;
 	struct mlx5_devx_create_rq_attr rq_attr = { 0 };
 	uint32_t cqn = rxq_ctrl->obj->devx_cq->id;
-	uint32_t wqe_size = 0;
-	uint32_t log_wqe_size = 0;
 	struct mlx5_devx_obj *rq;
 
 	/* Fill RQ attributes. */
-	rq_attr.mem_rq_type = MLX5_RQC_MEM_RQ_TYPE_MEMORY_RQ_INLINE;
-	rq_attr.flush_in_error_en = 1;
 	mlx5_devx_create_rq_attr_fill(rxq_data, cqn, &rq_attr);
 	rq_attr.ts_format = mlx5_ts_format_conv(priv->sh->rq_ts_format);
-	/* Fill WQ attributes for this RQ. */
-	if (mlx5_rxq_mprq_enabled(rxq_data)) {
-		rq_attr.wq_attr.wq_type = MLX5_WQ_TYPE_CYCLIC_STRIDING_RQ;
-		/*
-		 * Number of strides in each WQE:
-		 * 512*2^single_wqe_log_num_of_strides.
-		 */
-		rq_attr.wq_attr.single_wqe_log_num_of_strides =
-				rxq_data->strd_num_n -
-				MLX5_MIN_SINGLE_WQE_LOG_NUM_STRIDES;
-		/* Stride size = (2^single_stride_log_num_of_bytes)*64B. */
-		rq_attr.wq_attr.single_stride_log_num_of_bytes =
-				rxq_data->strd_sz_n -
-				MLX5_MIN_SINGLE_STRIDE_LOG_NUM_BYTES;
-		wqe_size = sizeof(struct mlx5_wqe_mprq);
+	if (rxq_data->shared) {
+		rq_attr.mem_rq_type = MLX5_RQC_MEM_RQ_TYPE_MEMORY_RQ_RMP;
+		rq_attr.rmpn = rxq_ctrl->obj->devx_rmp->id;
+		rq_attr.flush_in_error_en = 0;
 	} else {
-		rq_attr.wq_attr.wq_type = MLX5_WQ_TYPE_CYCLIC;
-		wqe_size = sizeof(struct mlx5_wqe_data_seg);
+		rq_attr.mem_rq_type = MLX5_RQC_MEM_RQ_TYPE_MEMORY_RQ_INLINE;
+		rq_attr.flush_in_error_en = 1;
+		mlx5_devx_wq_attr_fill(priv, rxq_ctrl, &rq_attr.wq_attr);
 	}
-	log_wqe_size = log2above(wqe_size) + rxq_data->sges_n;
-	rq_attr.wq_attr.log_wq_stride = log_wqe_size;
-	rq_attr.wq_attr.log_wq_sz = rxq_data->elts_n - rxq_data->sges_n;
-	/* Calculate and allocate WQ memory space. */
-	size_t alignment = MLX5_WQE_BUF_ALIGNMENT;
-	if (alignment == (size_t)-1) {
-		DRV_LOG(ERR, "Failed to get mem page size");
-		rte_errno = ENOMEM;
-		return NULL;
-	}
-	/* Create RQ using DevX API. */
-	mlx5_devx_wq_attr_fill(priv, rxq_ctrl, &rq_attr.wq_attr);
 	rq_attr.counter_set_id = priv->counter_set_id;
 	rq = mlx5_devx_cmd_create_rq(priv->sh->ctx, &rq_attr, rxq_ctrl->socket);
 	return rq;
@@ -712,8 +740,16 @@ mlx5_rxq_devx_obj_new(struct mlx5_rxq_priv *rxq)
 	if (ret != 0) {
 		DRV_LOG(ERR, "Port %u Rx queue %u RQ resources creation failure.",
 			priv->dev_data->port_id, rxq->idx);
-		rte_errno = ENOMEM;
 		goto error;
+	}
+	if (rxq_data->shared) {
+		/* Create receive memory pool. */
+		tmpl->devx_rmp = mlx5_rxq_create_devx_rmp(rxq);
+		if (tmpl->devx_rmp == NULL) {
+			DRV_LOG(ERR, "Port %u Rx queue %u RMP creation failure.",
+				priv->dev_data->port_id, rxq->idx);
+			goto error;
+		}
 	}
 	rxq_data->cq_arm_sn = 0;
 	mlx5_rxq_initialize(rxq_data);
@@ -721,6 +757,8 @@ mlx5_rxq_devx_obj_new(struct mlx5_rxq_priv *rxq)
 	return 0;
 error:
 	ret = rte_errno; /* Save rte_errno before cleanup. */
+	if (tmpl->devx_rmp != NULL)
+		claim_zero(mlx5_devx_cmd_destroy(tmpl->devx_rmp));
 	if (tmpl->devx_cq)
 		claim_zero(mlx5_devx_cmd_destroy(tmpl->devx_cq));
 	if (tmpl->devx_channel)

commit 9728e3f41b84656c0e7c68f5620665fd01f10040

net/mlx5: fix shared Rx queue re-configuration
When Rx queue shared by multiple ports, pre-setup didn't release all Rx
queues of member ports, the rxq_ctrl not destroyed and the setup simply
reuse existing Rx queue control object without new configuration. The
behavior is the new setup didn't take effect.

This patch release all Rx queues of shared Rx queue member ports in
pre-setup to properly clear saved setup before new configuration.

Fixes: c34e422b6830 ("net/mlx5: support receive memory pool")

Signed-off-by: Xueming Li <xuemingl@nvidia.com>

drivers/net/mlx5/linux/mlx5_verbs.c
@@ -472,6 +472,7 @@ mlx5_rxq_ibv_res_release(struct mlx5_rxq_priv *rxq)
 	if (rxq_obj->ibv_channel)
 		claim_zero(mlx5_glue->destroy_comp_channel
 							(rxq_obj->ibv_channel));
+	rxq->ctrl->started = false;
 }
 
 /**

drivers/net/mlx5/mlx5.h
@@ -1272,6 +1272,7 @@ struct mlx5_rxq_obj {
 		};
 		struct {
 			struct mlx5_devx_obj *devx_rmp; /* DevX RMP object. */
+			uint32_t rmp_refcnt; /* Reference count of RMP. */
 			struct mlx5_devx_cq cq_obj; /* DevX CQ object. */
 			void *devx_channel;
 		};

drivers/net/mlx5/mlx5_devx.c
@@ -214,8 +214,9 @@ mlx5_rxq_devx_res_release(struct mlx5_rxq_priv *rxq)
 		if (rxq->devx_rq) {
 			claim_zero(mlx5_devx_cmd_destroy(rxq->devx_rq));
 			rxq->devx_rq = NULL;
+			rxq_obj->rmp_refcnt--;
 		}
-		if (!RXQ_CTRL_LAST(rxq))
+		if (rxq_obj->rmp_refcnt != 0)
 			return;
 		if (rxq_obj->devx_rmp) {
 			claim_zero(mlx5_devx_cmd_destroy(rxq_obj->devx_rmp));
@@ -228,6 +229,7 @@ mlx5_rxq_devx_res_release(struct mlx5_rxq_priv *rxq)
 		}
 		mlx5_rxq_release_devx_resources(rxq_obj->rxq_ctrl);
 	}
+	rxq_ctrl->started = false;
 }
 
 /**
@@ -741,6 +743,7 @@ mlx5_rxq_devx_res_new(struct mlx5_rxq_priv *rxq)
 		rte_errno = ENOMEM;
 		goto error;
 	}
+	rxq->ctrl->obj->rmp_refcnt++;
 	/* Change queue state to ready. */
 	ret = mlx5_devx_modify_rq(rxq, MLX5_RXQ_MOD_RST2RDY);
 	if (ret)

drivers/net/mlx5/mlx5_rxq.c
@@ -687,14 +687,19 @@ mlx5_rx_queue_start(struct rte_eth_dev *dev, uint16_t idx)
  *   RX queue index.
  * @param desc
  *   Number of descriptors to configure in queue.
+ * @param[out] rxq_ctrl
+ *   Address of pointer to shared Rx queue control.
  *
  * @return
  *   0 on success, a negative errno value otherwise and rte_errno is set.
  */
 static int
-mlx5_rx_queue_pre_setup(struct rte_eth_dev *dev, uint16_t idx, uint16_t *desc)
+mlx5_rx_queue_pre_setup(struct rte_eth_dev *dev, uint16_t idx, uint16_t *desc,
+			struct mlx5_rxq_ctrl **rxq_ctrl)
 {
 	struct mlx5_priv *priv = dev->data->dev_private;
+	struct mlx5_rxq_priv *rxq;
+	bool empty;
 
 	if (!rte_is_power_of_2(*desc)) {
 		*desc = 1 << log2above(*desc);
@@ -711,13 +716,29 @@ mlx5_rx_queue_pre_setup(struct rte_eth_dev *dev, uint16_t idx, uint16_t *desc)
 		rte_errno = EOVERFLOW;
 		return -rte_errno;
 	}
-	if (!mlx5_rxq_releasable(dev, idx)) {
-		DRV_LOG(ERR, "port %u unable to release queue index %u",
-			dev->data->port_id, idx);
-		rte_errno = EBUSY;
-		return -rte_errno;
+	if (rxq_ctrl == NULL || *rxq_ctrl == NULL)
+		return 0;
+	if (!(*rxq_ctrl)->rxq.shared) {
+		if (!mlx5_rxq_releasable(dev, idx)) {
+			DRV_LOG(ERR, "port %u unable to release queue index %u",
+				dev->data->port_id, idx);
+			rte_errno = EBUSY;
+			return -rte_errno;
+		}
+		mlx5_rxq_release(dev, idx);
+	} else {
+		if ((*rxq_ctrl)->obj != NULL)
+			/* Some port using shared Rx queue has been started. */
+			return 0;
+		/* Release all owner RxQ to reconfigure Shared RxQ. */
+		do {
+			rxq = LIST_FIRST(&(*rxq_ctrl)->owners);
+			LIST_REMOVE(rxq, owner_entry);
+			empty = LIST_EMPTY(&(*rxq_ctrl)->owners);
+			mlx5_rxq_release(ETH_DEV(rxq->priv), rxq->idx);
+		} while (!empty);
+		*rxq_ctrl = NULL;
 	}
-	mlx5_rxq_release(dev, idx);
 	return 0;
 }
 
@@ -906,7 +927,7 @@ mlx5_rx_queue_setup(struct rte_eth_dev *dev, uint16_t idx, uint16_t desc,
 			return -rte_errno;
 		}
 	}
-	res = mlx5_rx_queue_pre_setup(dev, idx, &desc);
+	res = mlx5_rx_queue_pre_setup(dev, idx, &desc, &rxq_ctrl);
 	if (res)
 		return res;
 	/* Allocate RXQ. */
@@ -969,7 +990,7 @@ mlx5_rx_hairpin_queue_setup(struct rte_eth_dev *dev, uint16_t idx,
 	struct mlx5_rxq_ctrl *rxq_ctrl;
 	int res;
 
-	res = mlx5_rx_queue_pre_setup(dev, idx, &desc);
+	res = mlx5_rx_queue_pre_setup(dev, idx, &desc, NULL);
 	if (res)
 		return res;
 	if (hairpin_conf->peer_count != 1) {
@@ -1986,36 +2007,33 @@ mlx5_rxq_release(struct rte_eth_dev *dev, uint16_t idx)
 	struct mlx5_priv *priv = dev->data->dev_private;
 	struct mlx5_rxq_priv *rxq;
 	struct mlx5_rxq_ctrl *rxq_ctrl;
-	bool free_ctrl;
+	uint32_t refcnt;
 
 	if (priv->rxq_privs == NULL)
 		return 0;
 	rxq = mlx5_rxq_get(dev, idx);
-	if (rxq == NULL)
+	if (rxq == NULL || rxq->refcnt == 0)
 		return 0;
-	if (mlx5_rxq_deref(dev, idx) > 1)
-		return 1;
 	rxq_ctrl = rxq->ctrl;
-	/* If the last entry in share RXQ. */
-	free_ctrl = RXQ_CTRL_LAST(rxq);
-	if (rxq->devx_rq != NULL)
+	refcnt = mlx5_rxq_deref(dev, idx);
+	if (refcnt > 1) {
+		return 1;
+	} else if (refcnt == 1) { /* RxQ stopped. */
 		priv->obj_ops.rxq_res_release(rxq);
-	if (free_ctrl) {
-		if (rxq_ctrl->obj != NULL) {
+		if (!rxq_ctrl->started && rxq_ctrl->obj != NULL) {
 			LIST_REMOVE(rxq_ctrl->obj, next);
 			mlx5_free(rxq_ctrl->obj);
 			rxq_ctrl->obj = NULL;
 		}
-		rxq_ctrl->started = false;
-	}
-	if (rxq_ctrl->type == MLX5_RXQ_TYPE_STANDARD) {
-		if (free_ctrl)
-			rxq_free_elts(rxq_ctrl);
-		dev->data->rx_queue_state[idx] = RTE_ETH_QUEUE_STATE_STOPPED;
-	}
-	if (!__atomic_load_n(&rxq->refcnt, __ATOMIC_RELAXED)) {
+		if (rxq_ctrl->type == MLX5_RXQ_TYPE_STANDARD) {
+			if (!rxq_ctrl->started)
+				rxq_free_elts(rxq_ctrl);
+			dev->data->rx_queue_state[idx] =
+					RTE_ETH_QUEUE_STATE_STOPPED;
+		}
+	} else { /* Refcnt zero, closing device. */
 		LIST_REMOVE(rxq, owner_entry);
-		if (free_ctrl) {
+		if (LIST_EMPTY(&rxq_ctrl->owners)) {
 			if (rxq_ctrl->type == MLX5_RXQ_TYPE_STANDARD) {
 				mlx5_mr_btree_free
 					(&rxq_ctrl->rxq.mr_ctrl.cache_bh);