LiZhang-2020/dpdk

commit e1152faa47580b00e2f06f35635ae5f35156c1fb

net/mlx5: add flow queue flush
This commit adds the flow queue flush function.

Signed-off-by: Suanming Mou <suanmingm@nvidia.com>

drivers/net/mlx5/mlx5_flow_hw.c
@@ -212,11 +212,63 @@ flow_hw_q_dequeue(struct rte_eth_dev *dev,
 		job = (struct mlx5_hw_q_job *)res[i].user_data;
 		/* Restore user data. */
 		res[i].user_data = job->user_data;
-		if (job->type == MLX5_HW_Q_JOB_TYPE_DESTROY)
+		if (job->type == MLX5_HW_Q_JOB_TYPE_DESTROY) {
+			LIST_REMOVE(job->flow, next);
 			mlx5_ipool_free(job->flow->table->flow, job->flow->idx);
+		} else {
+			LIST_INSERT_HEAD(&priv->hw_q[queue].flow_list,
+					 job->flow, next);
+		}
 		priv->hw_q[queue].job[priv->hw_q[queue].job_idx++] = job;
 	}
 	return ret;
+}
+
+int
+flow_hw_q_flow_flush(struct rte_eth_dev *dev,
+		     uint32_t queue,
+		     struct rte_flow_error *error)
+{
+#define BURST_THR 32u
+	struct mlx5_priv *priv = dev->data->dev_private;
+	struct mlx5_hw_q *hw_q = &priv->hw_q[queue];
+	struct rte_flow_hw *flow, *flow_next;
+	struct rte_flow_q_ops_attr attr = {
+		.drain = 0,
+	};
+	struct rte_flow_q_op_res comp[BURST_THR];
+	int ret, i = 0, j;
+	uint32_t pending_rules = 0, flush_thr;
+
+	flush_thr = RTE_MIN(rte_align32prevpow2(hw_q->size), BURST_THR);
+	flow_next = LIST_FIRST(&hw_q->flow_list);
+	while (flow_next) {
+		flow = flow_next;
+		flow_next = LIST_NEXT(flow, next);
+		attr.drain = (((++i) % flush_thr == 0) ||
+			     (flow_next && (flow_next->table != flow->table)) ||
+			     !flow_next);
+		ret = flow_hw_q_flow_destroy(dev, queue, &attr,
+					     (struct rte_flow *)flow, error);
+		if (unlikely(ret))
+			return -1;
+		pending_rules++;
+		while (pending_rules >= flush_thr ||
+		       pending_rules >= hw_q->size ||
+		       (pending_rules && !flow_next)) {
+			ret = flow_hw_q_dequeue(dev, queue, comp,
+						flush_thr, error);
+			if (ret < 0)
+				return -1;
+			for (j = 0; j < ret; j++) {
+				if (comp[j].status == RTE_FLOW_Q_OP_ERROR)
+					return -1;
+			}
+			pending_rules -= ret;
+		}
+	}
+	MLX5_ASSERT(!pending_rules && LIST_EMPTY(&hw_q->flow_list));
+	return 0;
 }
 
 static int
@@ -674,9 +726,11 @@ flow_hw_configure(struct rte_eth_dev *dev,
 		rte_errno = ENOMEM;
 		goto err;
 	}
+	priv->nb_queues = port_attr->nb_queues;
 	for (i = 0; i < port_attr->nb_queues; i++) {
 		priv->hw_q[i].job_idx = queue_attr[i]->size;
 		priv->hw_q[i].size = queue_attr[i]->size;
+		LIST_INIT(&priv->hw_q[i].flow_list);
 		if (i == 0)
 			priv->hw_q[i].job = (struct mlx5_hw_q_job **)
 					    &priv->hw_q[port_attr->nb_queues];

drivers/net/mlx5/mlx5.h
@@ -338,6 +338,7 @@ struct mlx5_hw_q_job {
 struct mlx5_hw_q {
 	uint32_t job_idx; /* Free job index. */
 	uint32_t size; /* LIFO size. */
+	LIST_HEAD(flow_list, rte_flow_hw) flow_list; /* Flow list. */
 	struct mlx5_hw_q_job **job; /* LIFO pointer. */
 };
 
@@ -1539,6 +1540,7 @@ struct mlx5_priv {
 	/* Action template list. */
 	LIST_HEAD(flow_hw_at, rte_flow_action_template) flow_hw_at;
 	struct mlx5dr_context *dr_ctx; /**< HW steering DR context. */
+	uint32_t nb_queues; /* HW steering queue number. */
 	struct mlx5_hw_q *hw_q;
 	/**< HW steering queue polling mechanism job descriptor LIFO. */
 	LIST_HEAD(flow_hw_tbl, rte_flow_table) flow_hw_tbl;

drivers/net/mlx5/mlx5_flow.c
@@ -7226,7 +7226,15 @@ mlx5_flow_list_flush(struct rte_eth_dev *dev, enum mlx5_flow_type type,
 {
 	struct mlx5_priv *priv = dev->data->dev_private;
 	uint32_t num_flushed = 0, fidx = 1;
+	uint32_t queue;
 	struct rte_flow *flow;
+
+	if (priv->config.dv_flow_en == 2 &&
+	    type == MLX5_FLOW_TYPE_GEN) {
+		for (queue = 0; queue < priv->nb_queues; queue++)
+			flow_hw_q_flow_flush(dev, queue, NULL);
+		return;
+	}
 
 	MLX5_IPOOL_FOREACH(priv->flows[type], fidx, flow) {
 		mlx5_flow_list_destroy(dev, type, fidx);

drivers/net/mlx5/mlx5_flow.h
@@ -1117,6 +1117,7 @@ struct rte_flow {
 } __rte_packed;
 
 struct rte_flow_hw {
+	LIST_ENTRY(rte_flow_hw) next;
 	uint32_t idx;
 	struct rte_flow_table *table;
 	struct mlx5dr_rule rule;
@@ -1701,6 +1702,9 @@ mlx5_validate_integrity_item(const struct rte_flow_item_integrity *item)
 	return (test.value == 0);
 }
 
+int flow_hw_q_flow_flush(struct rte_eth_dev *dev,
+			 uint32_t queue,
+			 struct rte_flow_error *error);
 int mlx5_flow_group_to_table(struct rte_eth_dev *dev,
 			     const struct mlx5_flow_tunnel *tunnel,
 			     uint32_t group, uint32_t *table,

commit ba913da956cb15cc49b526d70224d6e4bc4d8971

net/mlx5: fix flow not dequeued issue
In case user forgot to dequeue the flows, these flows will be leaked
when port closes. And the extra left dequeued flows will cause flow
flush got more CQEs than expected pending flows. That finally caused
the pending flows be an invalid minus value after minus the CQE value
dequeque got.

This commit adds an extra drain and dequeue operation when port closes
to avoid flow leaked and extra CQEs.

Fixes: e1152faa4758 ("net/mlx5: add flow queue flush")

Signed-off-by: Suanming Mou <suanmingm@nvidia.com>

drivers/net/mlx5/mlx5_flow_hw.c
@@ -34,6 +34,10 @@ static uint32_t mlx5_hw_dr_ft_flag[2][MLX5DR_TABLE_TYPE_MAX] = {
 	},
 };
 
+static int
+flow_hw_q_drain(struct rte_eth_dev *dev, uint32_t queue,
+		struct rte_flow_error *error);
+
 static void
 flow_hw_release_jump(struct rte_eth_dev *dev, struct mlx5_hw_jump_action *jump)
 {
@@ -1236,6 +1240,15 @@ flow_hw_q_flow_flush(struct rte_eth_dev *dev,
 	int ret, i = 0, j;
 	uint32_t pending_rules = 0, flush_thr;
 
+	/*
+	 * Ensure to drain and dequeue all the enqueued flows in case user
+	 * forgot to dequeue. Or the enqueued created flows will be leaked.
+	 * The forgot dequeue will also cause flow flush get extra CQEs as
+	 * expected and pending_rules be minus value.
+	 */
+	flow_hw_q_drain(dev, queue, error);
+	while (priv->hw_q[queue].size != priv->hw_q[queue].job_idx)
+		flow_hw_q_dequeue(dev, queue, comp, BURST_THR, error);
 	flush_thr = RTE_MIN(rte_align32prevpow2(hw_q->size), BURST_THR);
 	flow_next = LIST_FIRST(&hw_q->flow_list);
 	while (flow_next) {
@@ -1259,6 +1272,10 @@ flow_hw_q_flow_flush(struct rte_eth_dev *dev,
 			for (j = 0; j < ret; j++) {
 				if (comp[j].status == RTE_FLOW_Q_OP_ERROR)
 					return -1;
+			}
+			if ((uint32_t)ret > pending_rules) {
+				DRV_LOG(WARNING, "Flow flush get extra CQE.");
+				return -1;
 			}
 			pending_rules -= ret;
 		}