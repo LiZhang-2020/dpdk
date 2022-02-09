LiZhang-2020/dpdk

commit 1932f245d41e9c207cdf8c787406c329ba2a2242

net/mlx5: add table management callbacks
Flow table is a group of flows with the same matching criteria
and the same actions defined for them. The table defines rules
that have the same matching fields but with different matching
values. For example, matching on 5 tuple, the table will be
(IPv4 source + IPv4 dest + s_port + d_port + next_proto)
while the values for each rule will be different.

The templates' relevant matching criteria and action instances
will be created in the table creation and saved in the table.
As table attributes indicate the supported flow number, the flow
memory will also be allocated at the same time.

This commit adds the table management functions.

Signed-off-by: Suanming Mou <suanmingm@nvidia.com>

drivers/net/mlx5/mlx5_flow_hw.c
@@ -8,6 +8,220 @@
 
 const struct mlx5_flow_driver_ops mlx5_flow_hw_drv_ops;
 
+static uint32_t mlx5_hw_dr_ft_flag[2][MLX5DR_TABLE_TYPE_MAX] = {
+	{
+		MLX5DR_ACTION_FLAG_ROOT_RX,
+		MLX5DR_ACTION_FLAG_ROOT_TX,
+		MLX5DR_ACTION_FLAG_ROOT_FDB,
+	},
+	{
+		MLX5DR_ACTION_FLAG_HWS_RX,
+		MLX5DR_ACTION_FLAG_HWS_TX,
+		MLX5DR_ACTION_FLAG_HWS_FDB,
+	},
+};
+
+static void
+__flow_hw_action_template_destroy(struct mlx5_hw_actions *acts __rte_unused)
+{
+}
+
+static int
+flow_hw_actions_translate(struct rte_eth_dev *dev,
+			  const struct rte_flow_table_attr *table_attr,
+			  struct mlx5_hw_actions *acts,
+			  struct rte_flow_action_template *at,
+			  struct rte_flow_error *error __rte_unused)
+{
+	struct mlx5_priv *priv = dev->data->dev_private;
+	const struct rte_flow_attr *attr = &table_attr->flow_attr;
+	struct rte_flow_action *actions = at->actions;
+	struct rte_flow_action *masks = at->masks;
+	bool actions_end = false;
+	uint32_t type;
+
+	if (attr->transfer)
+		type = MLX5DR_TABLE_TYPE_FDB;
+	else if (attr->egress)
+		type = MLX5DR_TABLE_TYPE_NIC_TX;
+	else
+		type = MLX5DR_TABLE_TYPE_NIC_RX;
+	for (; !actions_end; actions++, masks++) {
+		switch (actions->type) {
+		case RTE_FLOW_ACTION_TYPE_INDIRECT:
+			break;
+		case RTE_FLOW_ACTION_TYPE_VOID:
+			break;
+		case RTE_FLOW_ACTION_TYPE_DROP:
+			acts->drop = priv->hw_drop[!!attr->group][type];
+			break;
+		case RTE_FLOW_ACTION_TYPE_END:
+			actions_end = true;
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+}
+
+static struct rte_flow_table *
+flow_hw_table_create(struct rte_eth_dev *dev,
+		     const struct rte_flow_table_attr *attr,
+		     struct rte_flow_item_template *item_templates[],
+		     uint8_t nb_item_templates,
+		     struct rte_flow_action_template *action_templates[],
+		     uint8_t nb_action_templates,
+		     struct rte_flow_error *error)
+{
+	struct mlx5_priv *priv = dev->data->dev_private;
+	struct mlx5dr_matcher_attr matcher_attr = {0};
+	struct rte_flow_table *tbl = NULL;
+	struct mlx5_flow_group *grp;
+	struct mlx5dr_match_template *mt[MLX5_HW_TBL_MAX_ITEM_TEMPLATE];
+	struct rte_flow_attr flow_attr = attr->flow_attr;
+	struct mlx5_flow_cb_ctx ctx = {
+		.dev = dev,
+		.error = error,
+		.data = &flow_attr,
+	};
+	struct mlx5_indexed_pool_config cfg = {
+		.size = sizeof(struct rte_flow),
+		.trunk_size = 1 << 12,
+		.per_core_cache = 1 << 13,
+		.need_lock = 1,
+		.release_mem_en = !!priv->config.reclaim_mode,
+		.malloc = mlx5_malloc,
+		.free = mlx5_free,
+		.type = "mlx5_hw_table_flow",
+	};
+	struct mlx5_list_entry *ge;
+	uint32_t i, max_tpl = MLX5_HW_TBL_MAX_ITEM_TEMPLATE;
+	uint32_t nb_flows = rte_align32pow2(attr->nb_flows);
+	int err;
+
+	if (!attr->flow_attr.group)
+		max_tpl = 1;
+	cfg.max_idx = nb_flows;
+	if (nb_flows < cfg.trunk_size) {
+		cfg.per_core_cache = nb_flows >> 2;
+		cfg.trunk_size = nb_flows;
+	}
+	if (nb_item_templates > max_tpl ||
+	    nb_action_templates > MLX5_HW_TBL_MAX_ACTION_TEMPLATE) {
+		rte_errno = EINVAL;
+		goto error;
+	}
+	/* Allocate the table memory. */
+	tbl = mlx5_malloc(MLX5_MEM_ZERO, sizeof(*tbl), 0, SOCKET_ID_ANY);
+	if (!tbl)
+		goto error;
+	/* Allocate flow indexed pool. */
+	tbl->flow = mlx5_ipool_create(&cfg);
+	if (!tbl->flow)
+		goto error;
+	/* Register the flow table. */
+	ge = mlx5_hlist_register(priv->sh->groups, attr->flow_attr.group, &ctx);
+	if (!ge)
+		goto error;
+	grp = container_of(ge, struct mlx5_flow_group, entry);
+	tbl->grp = grp;
+	/* Prepare matcher information. */
+	matcher_attr.priority = attr->flow_attr.priority;
+	matcher_attr.mode = MLX5DR_MATCHER_RESOURCE_MODE_RULE;
+	matcher_attr.rule.num_log = rte_log2_u32(nb_flows);
+	/* Build the item template. */
+	for (i = 0; i < nb_item_templates; i++) {
+		uint32_t ret;
+
+		ret = __atomic_add_fetch(&item_templates[i]->refcnt, 1,
+					 __ATOMIC_RELAXED);
+		if (ret <= 1) {
+			rte_errno = EINVAL;
+			goto it_error;
+		}
+		mt[i] = item_templates[i]->mt;
+		tbl->its[i] = item_templates[i];
+	}
+	tbl->matcher = mlx5dr_matcher_create
+		(tbl->grp->tbl, mt, nb_item_templates, &matcher_attr);
+	if (!tbl->matcher)
+		goto it_error;
+	tbl->nb_item_templates = nb_item_templates;
+	/* Build the action template. */
+	for (i = 0; i < nb_action_templates; i++) {
+		uint32_t ret;
+
+		ret = __atomic_add_fetch(&action_templates[i]->refcnt, 1,
+					 __ATOMIC_RELAXED);
+		if (ret <= 1) {
+			rte_errno = EINVAL;
+			goto at_error;
+		}
+		err = flow_hw_actions_translate(dev, attr,
+						&tbl->ats[i].acts,
+						action_templates[i], error);
+		if (err)
+			goto at_error;
+		tbl->ats[i].action_template = action_templates[i];
+	}
+	tbl->nb_action_templates = nb_action_templates;
+	tbl->type = attr->flow_attr.transfer ? MLX5DR_TABLE_TYPE_FDB :
+		    (attr->flow_attr.egress ? MLX5DR_TABLE_TYPE_NIC_TX :
+		    MLX5DR_TABLE_TYPE_NIC_RX);
+	LIST_INSERT_HEAD(&priv->flow_hw_tbl, tbl, next);
+	return tbl;
+at_error:
+	while (i--) {
+		__flow_hw_action_template_destroy(&tbl->ats[i].acts);
+		__atomic_sub_fetch(&action_templates[i]->refcnt,
+				   1, __ATOMIC_RELAXED);
+	}
+	i = nb_item_templates;
+it_error:
+	while (i--)
+		__atomic_sub_fetch(&item_templates[i]->refcnt,
+				   1, __ATOMIC_RELAXED);
+	mlx5dr_matcher_destroy(tbl->matcher);
+error:
+	err = rte_errno;
+	if (tbl) {
+		if (tbl->flow)
+			mlx5_ipool_destroy(tbl->flow);
+		mlx5_free(tbl);
+	}
+	rte_flow_error_set(error, err,
+			  RTE_FLOW_ERROR_TYPE_UNSPECIFIED, NULL,
+			  "fail to create rte table");
+	return NULL;
+}
+
+static int
+flow_hw_table_destroy(struct rte_eth_dev *dev,
+		      struct rte_flow_table *table,
+		      struct rte_flow_error *error __rte_unused)
+{
+	struct mlx5_priv *priv = dev->data->dev_private;
+	int i;
+
+	if (table->refcnt)
+		DRV_LOG(WARNING, "Table %p is still in use.", (void *)table);
+	LIST_REMOVE(table, next);
+	for (i = 0; i < table->nb_item_templates; i++)
+		__atomic_sub_fetch(&table->its[i]->refcnt,
+				   1, __ATOMIC_RELAXED);
+	for (i = 0; i < table->nb_action_templates; i++) {
+		__flow_hw_action_template_destroy(&table->ats[i].acts);
+		__atomic_sub_fetch(&table->ats[i].action_template->refcnt,
+				   1, __ATOMIC_RELAXED);
+	}
+	mlx5dr_matcher_destroy(table->matcher);
+	mlx5_hlist_unregister(priv->sh->groups, &table->grp->entry);
+	mlx5_ipool_destroy(table->flow);
+	mlx5_free(table);
+	return 0;
+}
+
 static struct rte_flow_action_template *
 flow_hw_action_template_create(struct rte_eth_dev *dev,
 			       const struct rte_flow_action_template_attr *attr,
@@ -121,6 +335,143 @@ flow_hw_item_template_destroy(struct rte_eth_dev *dev __rte_unused,
 	return 0;
 }
 
+struct mlx5_list_entry *
+flow_hw_grp_create_cb(void *tool_ctx, void *cb_ctx)
+{
+	struct mlx5_dev_ctx_shared *sh = tool_ctx;
+	struct mlx5_flow_cb_ctx *ctx = cb_ctx;
+	struct rte_eth_dev *dev = ctx->dev;
+	struct rte_flow_attr *attr = (struct rte_flow_attr *)ctx->data;
+	struct mlx5_priv *priv = dev->data->dev_private;
+	struct mlx5dr_table_attr dr_tbl_attr = {0};
+	struct rte_flow_error *error = ctx->error;
+	struct mlx5_flow_group *grp_data;
+	struct mlx5dr_table *tbl = NULL;
+	struct mlx5dr_action *jump;
+	uint32_t idx = 0;
+
+	grp_data = mlx5_ipool_zmalloc(sh->ipool[MLX5_IPOOL_HW_GRP], &idx);
+	if (!grp_data) {
+		rte_flow_error_set(error, ENOMEM,
+				   RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+				   NULL,
+				   "cannot allocate flow table data entry");
+		return NULL;
+	}
+	dr_tbl_attr.level = attr->group;
+	if (attr->transfer)
+		dr_tbl_attr.type = MLX5DR_TABLE_TYPE_FDB;
+	else if (attr->egress)
+		dr_tbl_attr.type = MLX5DR_TABLE_TYPE_NIC_TX;
+	else
+		dr_tbl_attr.type = MLX5DR_TABLE_TYPE_NIC_RX;
+	tbl = mlx5dr_table_create(priv->dr_ctx, &dr_tbl_attr);
+	if (!tbl)
+		goto error;
+	grp_data->tbl = tbl;
+	if (attr->group) {
+		jump = mlx5dr_action_create_dest_table
+			(priv->dr_ctx, tbl,
+			 mlx5_hw_dr_ft_flag[!!attr->group][dr_tbl_attr.type]);
+		if (!jump)
+			goto error;
+		grp_data->jump.hws_action = jump;
+		jump = mlx5dr_action_create_dest_table
+			(priv->dr_ctx, tbl,
+			 mlx5_hw_dr_ft_flag[0][dr_tbl_attr.type]);
+		if (!jump)
+			goto error;
+		grp_data->jump.root_action = jump;
+	}
+	grp_data->idx = idx;
+	grp_data->group_id = attr->group;
+	grp_data->type = dr_tbl_attr.type;
+	return &grp_data->entry;
+error:
+	if (grp_data->jump.root_action)
+		mlx5dr_action_destroy(grp_data->jump.root_action);
+	if (grp_data->jump.hws_action)
+		mlx5dr_action_destroy(grp_data->jump.hws_action);
+	if (tbl)
+		mlx5dr_table_destroy(tbl);
+	if (idx)
+		mlx5_ipool_free(sh->ipool[MLX5_IPOOL_HW_GRP], idx);
+	rte_flow_error_set(error, ENOMEM,
+			   RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+			   NULL,
+			   "cannot allocate flow dr table");
+	return NULL;
+}
+
+void
+flow_hw_grp_remove_cb(void *tool_ctx, struct mlx5_list_entry *entry)
+{
+	struct mlx5_dev_ctx_shared *sh = tool_ctx;
+	struct mlx5_flow_group *grp_data =
+		    container_of(entry, struct mlx5_flow_group, entry);
+
+	MLX5_ASSERT(entry && sh);
+	/* To use the wrapper glue functions instead. */
+	if (grp_data->jump.hws_action)
+		mlx5dr_action_destroy(grp_data->jump.hws_action);
+	if (grp_data->jump.root_action)
+		mlx5dr_action_destroy(grp_data->jump.root_action);
+	mlx5dr_table_destroy(grp_data->tbl);
+	mlx5_ipool_free(sh->ipool[MLX5_IPOOL_HW_GRP], grp_data->idx);
+}
+
+int
+flow_hw_grp_match_cb(void *tool_ctx __rte_unused, struct mlx5_list_entry *entry,
+		     void *cb_ctx)
+{
+	struct mlx5_flow_cb_ctx *ctx = cb_ctx;
+	struct mlx5_flow_group *grp_data =
+		container_of(entry, struct mlx5_flow_group, entry);
+	struct rte_flow_attr *attr =
+			(struct rte_flow_attr *)ctx->data;
+
+	return (grp_data->group_id != attr->group) ||
+		((grp_data->type != MLX5DR_TABLE_TYPE_FDB) &&
+		attr->transfer) ||
+		((grp_data->type != MLX5DR_TABLE_TYPE_NIC_TX) &&
+		attr->egress) ||
+		((grp_data->type != MLX5DR_TABLE_TYPE_NIC_RX) &&
+		attr->ingress);
+}
+
+struct mlx5_list_entry *
+flow_hw_grp_clone_cb(void *tool_ctx, struct mlx5_list_entry *oentry,
+		     void *cb_ctx)
+{
+	struct mlx5_dev_ctx_shared *sh = tool_ctx;
+	struct mlx5_flow_cb_ctx *ctx = cb_ctx;
+	struct mlx5_flow_group *grp_data;
+	struct rte_flow_error *error = ctx->error;
+	uint32_t idx = 0;
+
+	grp_data = mlx5_ipool_malloc(sh->ipool[MLX5_IPOOL_HW_GRP], &idx);
+	if (!grp_data) {
+		rte_flow_error_set(error, ENOMEM,
+				   RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+				   NULL,
+				   "cannot allocate flow table data entry");
+		return NULL;
+	}
+	memcpy(grp_data, oentry, sizeof(*grp_data));
+	grp_data->idx = idx;
+	return &grp_data->entry;
+}
+
+void
+flow_hw_grp_clone_free_cb(void *tool_ctx, struct mlx5_list_entry *entry)
+{
+	struct mlx5_dev_ctx_shared *sh = tool_ctx;
+	struct mlx5_flow_group *grp_data =
+		    container_of(entry, struct mlx5_flow_group, entry);
+
+	mlx5_ipool_free(sh->ipool[MLX5_IPOOL_HW_GRP], grp_data->idx);
+}
+
 static int
 flow_hw_configure(struct rte_eth_dev *dev,
 		  const struct rte_flow_port_attr *port_attr,
@@ -171,8 +522,24 @@ flow_hw_configure(struct rte_eth_dev *dev,
 		for (j = 0; j < queue_attr[i]->size; j++)
 			priv->hw_q[i].job[j] = &job[j];
 	}
+	/* Add global actions. */
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < MLX5DR_TABLE_TYPE_MAX; j++) {
+			priv->hw_drop[i][j] = mlx5dr_action_create_dest_drop
+				(priv->dr_ctx, mlx5_hw_dr_ft_flag[i][j]);
+			if (!priv->hw_drop[i][j])
+				goto err;
+		}
+	}
 	return 0;
 err:
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < MLX5DR_TABLE_TYPE_MAX; j++) {
+			if (!priv->hw_drop[i][j])
+				continue;
+			mlx5dr_action_destroy(priv->hw_drop[i][j]);
+		}
+	}
 	if (dr_ctx)
 		claim_zero(mlx5dr_context_close(dr_ctx));
 	return rte_flow_error_set(err, rte_errno,
@@ -184,11 +551,16 @@ void
 flow_hw_resource_release(struct rte_eth_dev *dev)
 {
 	struct mlx5_priv *priv = dev->data->dev_private;
+	struct rte_flow_table *tbl;
 	struct rte_flow_item_template *it;
 	struct rte_flow_action_template *at;
 
 	if (!priv->dr_ctx)
 		return;
+	while (!LIST_EMPTY(&priv->flow_hw_tbl)) {
+		tbl = LIST_FIRST(&priv->flow_hw_tbl);
+		flow_hw_table_destroy(dev, tbl, NULL);
+	}
 	while (!LIST_EMPTY(&priv->flow_hw_itt)) {
 		it = LIST_FIRST(&priv->flow_hw_itt);
 		flow_hw_item_template_destroy(dev, it, NULL);
@@ -207,4 +579,6 @@ const struct mlx5_flow_driver_ops mlx5_flow_hw_drv_ops = {
 	.item_template_destroy = flow_hw_item_template_destroy,
 	.action_template_create = flow_hw_action_template_create,
 	.action_template_destroy = flow_hw_action_template_destroy,
+	.table_create = flow_hw_table_create,
+	.table_destroy = flow_hw_table_destroy,
 };

drivers/net/mlx5/mlx5.c
@@ -1355,12 +1355,38 @@ void
 mlx5_free_table_hash_list(struct mlx5_priv *priv)
 {
 	struct mlx5_dev_ctx_shared *sh = priv->sh;
-
-	if (!sh->flow_tbls)
+	struct mlx5_hlist **tbls = (priv->config.dv_flow_en == 2) ?
+				   &sh->groups : &sh->flow_tbls;
+	if (*tbls)
 		return;
-	mlx5_hlist_destroy(sh->flow_tbls);
-	sh->flow_tbls = NULL;
+	mlx5_hlist_destroy(*tbls);
+	*tbls = NULL;
 }
+
+static int
+mlx5_alloc_hw_group_hash_list(struct mlx5_priv *priv)
+{
+	int err = 0;
+	struct mlx5_dev_ctx_shared *sh = priv->sh;
+	char s[MLX5_NAME_SIZE];
+
+	MLX5_ASSERT(sh);
+	snprintf(s, sizeof(s), "%s_flow_groups", priv->sh->ibdev_name);
+	sh->groups = mlx5_hlist_create
+			(s, MLX5_FLOW_TABLE_HLIST_ARRAY_SIZE,
+			 false, true, sh,
+			 flow_hw_grp_create_cb,
+			 flow_hw_grp_match_cb,
+			 flow_hw_grp_remove_cb,
+			 flow_hw_grp_clone_cb,
+			 flow_hw_grp_clone_free_cb);
+	if (!sh->groups) {
+		DRV_LOG(ERR, "flow groups with hash creation failed.");
+		err = ENOMEM;
+	}
+	return err;
+}
+
 
 /**
  * Initialize flow table hash list and create the root tables entry
@@ -1373,9 +1399,12 @@ mlx5_free_table_hash_list(struct mlx5_priv *priv)
  *   Zero on success, positive error code otherwise.
  */
 int
-mlx5_alloc_table_hash_list(struct mlx5_priv *priv __rte_unused)
+mlx5_alloc_table_hash_list(struct mlx5_priv *priv)
 {
 	int err = 0;
+
+	if (priv->config.dv_flow_en == 2)
+		return mlx5_alloc_hw_group_hash_list(priv);
 	/* Tables are only used in DV and DR modes. */
 #ifdef HAVE_IBV_FLOW_DV_SUPPORT
 	struct mlx5_dev_ctx_shared *sh = priv->sh;

drivers/net/mlx5/mlx5.h
@@ -64,6 +64,7 @@ enum mlx5_ipool_index {
 	MLX5_IPOOL_TAG, /* Pool for tag resource. */
 	MLX5_IPOOL_PORT_ID, /* Pool for port id resource. */
 	MLX5_IPOOL_JUMP, /* Pool for jump resource. */
+	MLX5_IPOOL_HW_GRP = MLX5_IPOOL_JUMP,
 	MLX5_IPOOL_SAMPLE, /* Pool for sample resource. */
 	MLX5_IPOOL_DEST_ARRAY, /* Pool for destination array resource. */
 	MLX5_IPOOL_TUNNEL_ID, /* Pool for tunnel offload context */
@@ -257,7 +258,7 @@ struct mlx5_dev_config {
 	unsigned int l3_vxlan_en:1; /* Enable L3 VXLAN flow creation. */
 	unsigned int vf_nl_en:1; /* Enable Netlink requests in VF mode. */
 	unsigned int dv_esw_en:1; /* Enable E-Switch DV flow. */
-	unsigned int dv_flow_en:1; /* Enable DV flow. */
+	unsigned int dv_flow_en:2; /* Enable DV flow. */
 	unsigned int dv_xmeta_en:2; /* Enable extensive flow metadata. */
 	unsigned int dv_validate_mod:1; /* Validate on modify actions */
 	unsigned int lacp_by_user:1;
@@ -1210,7 +1211,10 @@ struct mlx5_dev_ctx_shared {
 	rte_spinlock_t uar_lock[MLX5_UAR_PAGE_NUM_MAX];
 	/* UAR same-page access control required in 32bit implementations. */
 #endif
-	struct mlx5_hlist *flow_tbls;
+	union {
+		struct mlx5_hlist *flow_tbls;
+		struct mlx5_hlist *groups;
+	};
 	struct mlx5_hlist *flow_sfts;
 	struct mlx5_hlist *sft_zones_hash;
 	struct mlx5_flow_tunnel_hub *tunnel_hub;
@@ -1480,6 +1484,7 @@ struct mlx5_priv {
 	unsigned int reta_idx_n; /* RETA index size. */
 	struct mlx5_drop drop_queue; /* Flow drop queues. */
 	void *root_drop_action; /* Pointer to root drop action. */
+	struct mlx5dr_action *hw_drop[2][MLX5DR_TABLE_TYPE_MAX];
 	struct mlx5_indexed_pool *flows[MLX5_FLOW_TYPE_MAXI];
 	/* RTE Flow rules. */
 	uint32_t ctrl_flows; /* Control flow rules. */
@@ -1536,6 +1541,8 @@ struct mlx5_priv {
 	struct mlx5dr_context *dr_ctx; /**< HW steering DR context. */
 	struct mlx5_hw_q *hw_q;
 	/**< HW steering queue polling mechanism job descriptor LIFO. */
+	LIST_HEAD(flow_hw_tbl, rte_flow_table) flow_hw_tbl;
+	/**< HW steering rte flow table list header. */
 };
 
 #define PORT_ID(priv) ((priv)->dev_data->port_id)

drivers/net/mlx5/mlx5_flow.c
@@ -823,6 +823,19 @@ mlx5_flow_action_template_destroy(struct rte_eth_dev *dev,
 				  struct rte_flow_action_template *template,
 				  struct rte_flow_error *error);
 
+static struct rte_flow_table *
+mlx5_flow_table_create(struct rte_eth_dev *dev,
+		       const struct rte_flow_table_attr *attr,
+		       struct rte_flow_item_template *item_templates[],
+		       uint8_t nb_item_templates,
+		       struct rte_flow_action_template *action_templates[],
+		       uint8_t nb_action_templates,
+		       struct rte_flow_error *error);
+static int
+mlx5_flow_table_destroy(struct rte_eth_dev *dev,
+			struct rte_flow_table *table,
+			struct rte_flow_error *error);
+
 static const struct rte_flow_ops mlx5_flow_ops = {
 	.validate = mlx5_flow_validate,
 	.create = mlx5_flow_create,
@@ -848,6 +861,8 @@ static const struct rte_flow_ops mlx5_flow_ops = {
 	.item_template_destroy = mlx5_flow_item_template_destroy,
 	.action_template_create = mlx5_flow_action_template_create,
 	.action_template_destroy = mlx5_flow_action_template_destroy,
+	.table_create = mlx5_flow_table_create,
+	.table_destroy = mlx5_flow_table_destroy,
 };
 
 /* Tunnel information. */
@@ -8194,6 +8209,35 @@ mlx5_flow_action_template_destroy(struct rte_eth_dev *dev,
 			flow_get_drv_ops(MLX5_FLOW_TYPE_HW);
 
 	return fops->action_template_destroy(dev, template, error);
+}
+
+static struct rte_flow_table *
+mlx5_flow_table_create(struct rte_eth_dev *dev,
+		       const struct rte_flow_table_attr *attr,
+		       struct rte_flow_item_template *item_templates[],
+		       uint8_t nb_item_templates,
+		       struct rte_flow_action_template *action_templates[],
+		       uint8_t nb_action_templates,
+		       struct rte_flow_error *error)
+{
+	const struct mlx5_flow_driver_ops *fops =
+			flow_get_drv_ops(MLX5_FLOW_TYPE_HW);
+
+	return fops->table_create(dev, attr,
+				  item_templates, nb_item_templates,
+				  action_templates, nb_action_templates,
+				  error);
+}
+
+static int
+mlx5_flow_table_destroy(struct rte_eth_dev *dev,
+			struct rte_flow_table *table,
+			struct rte_flow_error *error)
+{
+	const struct mlx5_flow_driver_ops *fops =
+			flow_get_drv_ops(MLX5_FLOW_TYPE_HW);
+
+	return fops->table_destroy(dev, table, error);
 }
 
 /**

drivers/net/mlx5/mlx5_flow.h
@@ -1132,6 +1132,50 @@ struct rte_flow_action_template {
 	uint32_t refcnt; /* Reference counter. */
 };
 
+struct mlx5_hw_jump_action {
+	/* Action jump from root. */
+	struct mlx5dr_action *root_action;
+	/* HW steering jump action. */
+	struct mlx5dr_action *hws_action;
+};
+
+struct mlx5_hw_actions {
+	struct mlx5dr_action *drop; /* Drop action. */
+};
+
+struct mlx5_hw_action_template {
+	/* Action template pointer. */
+	struct rte_flow_action_template *action_template;
+	struct mlx5_hw_actions acts; /* Template actions. */
+};
+
+struct mlx5_flow_group {
+	struct mlx5_list_entry entry;
+	struct mlx5dr_table *tbl; /* HWS table object. */
+	struct mlx5_hw_jump_action jump; /* Jump action. */
+	enum mlx5dr_table_type type; /* Table type. */
+	uint32_t group_id; /* Group id. */
+	uint32_t idx; /* Group memory index. */
+};
+
+#define MLX5_HW_TBL_MAX_ITEM_TEMPLATE 2
+#define MLX5_HW_TBL_MAX_ACTION_TEMPLATE 32
+
+struct rte_flow_table {
+	LIST_ENTRY(rte_flow_table) next;
+	struct mlx5_flow_group *grp; /* The group rte_flow_table uses. */
+	struct mlx5dr_matcher *matcher; /* Template matcher. */
+	/* Item templates bind to the table. */
+	struct rte_flow_item_template *its[MLX5_HW_TBL_MAX_ITEM_TEMPLATE];
+	/* Action templates bind to the table. */
+	struct mlx5_hw_action_template ats[MLX5_HW_TBL_MAX_ACTION_TEMPLATE];
+	struct mlx5_indexed_pool *flow; /* The table's flow ipool. */
+	uint32_t type; /* Flow table type RX/TX/FDB. */
+	uint8_t nb_item_templates; /* Item template number. */
+	uint8_t nb_action_templates; /* Action template number. */
+	uint32_t refcnt; /* Table reference counter. */
+};
+
 /*
  * Define list of valid combinations of RX Hash fields
  * (see enum ibv_rx_hash_fields).
@@ -1401,6 +1445,18 @@ typedef int (*mlx5_flow_action_template_destroy_t)
 			(struct rte_eth_dev *dev,
 			 struct rte_flow_action_template *template,
 			 struct rte_flow_error *error);
+typedef struct rte_flow_table *(*mlx5_flow_table_create_t)
+		(struct rte_eth_dev *dev,
+		 const struct rte_flow_table_attr *attr,
+		 struct rte_flow_item_template *item_templates[],
+		 uint8_t nb_item_templates,
+		 struct rte_flow_action_template *action_templates[],
+		 uint8_t nb_action_templates,
+		 struct rte_flow_error *error);
+typedef int (*mlx5_flow_table_destroy_t)
+			(struct rte_eth_dev *dev,
+			 struct rte_flow_table *table,
+			 struct rte_flow_error *error);
 
 struct mlx5_flow_driver_ops {
 	mlx5_flow_validate_t validate;
@@ -1444,6 +1500,8 @@ struct mlx5_flow_driver_ops {
 	mlx5_flow_item_template_destroy_t item_template_destroy;
 	mlx5_flow_action_template_create_t action_template_create;
 	mlx5_flow_action_template_destroy_t action_template_destroy;
+	mlx5_flow_table_create_t table_create;
+	mlx5_flow_table_destroy_t table_destroy;
 };
 
 /* mlx5_flow.c */
@@ -1891,6 +1949,15 @@ int flow_dv_query_count_ptr(struct rte_eth_dev *dev, uint32_t cnt_idx,
 		void **action, struct rte_flow_error *error);
 int flow_dv_query_count(const struct rte_eth_dev *dev, uint32_t cnt_idx,
 		void *data, struct rte_flow_error *error);
+struct mlx5_list_entry *flow_hw_grp_create_cb(void *tool_ctx, void *cb_ctx);
+void flow_hw_grp_remove_cb(void *tool_ctx, struct mlx5_list_entry *entry);
+int flow_hw_grp_match_cb(void *tool_ctx,
+			 struct mlx5_list_entry *entry,
+			 void *cb_ctx);
+struct mlx5_list_entry *flow_hw_grp_clone_cb(void *tool_ctx,
+					     struct mlx5_list_entry *oentry,
+					     void *cb_ctx);
+void flow_hw_grp_clone_free_cb(void *tool_ctx, struct mlx5_list_entry *entry);
 
 struct mlx5_aso_age_action *flow_aso_age_get_by_idx
 			(const struct rte_eth_dev *dev, uint32_t age_idx);

commit 05e264ca3431a12aa7e5bb43efdd259c954175d4

net/mlx5: fix group entry leakage
This commit releaes the allocated HWS group entry while creation failed.

Fixes: 1932f245d41e ("net/mlx5: add table management callbacks")
Signed-off-by: Suanming Mou <suanmingm@nvidia.com>

drivers/net/mlx5/mlx5_flow_hw.c
@@ -1509,6 +1509,9 @@ it_error:
 error:
 	err = rte_errno;
 	if (tbl) {
+		if (tbl->grp)
+			mlx5_hlist_unregister(priv->sh->groups,
+					      &tbl->grp->entry);
 		if (tbl->flow)
 			mlx5_ipool_destroy(tbl->flow);
 		mlx5_free(tbl);