a07def3a47bf5193175d46f97deec9a87c435f4e
Michael Baum <michaelba@nvidia.com>
2022-02-14 11:34:52 +0200

    net/mlx5: fix check sibling device config mismatch
    The MLX5 net driver supports "probe again". In probing again, it
    creates a new ethdev under an existing infiniband device context.
    
    Sibling devices sharing infiniband device context should have compatible
    configurations, so some of the devargs given in the probe again, the
    ones that are mainly relevant to the sharing device context are sent to
    the mlx5_dev_check_sibling_config function which makes sure that they
    compatible its siblings.
    However, the arguments are adjusted according to the capability of the
    device, and the function compares the arguments of the probe again
    before the adjustment with the arguments of the siblings after the
    adjustment. A user who sends the same values to all siblings may fail in
    this comparison if he requested something that the device does not
    support and adjusted.
    
    This patch moves the call to the mlx5_dev_check_sibling_config function
    after the relevant adjustments.
    
    Fixes: 92d5dd483450 ("net/mlx5: check sibling device configurations mismatch")
    Fixes: 2d241515ebaf ("net/mlx5: add devarg for extensive metadata support")
    Cc: stable@dpdk.org
    
    Signed-off-by: Michael Baum <michaelba@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>

--- drivers/net/mlx5/linux/mlx5_os.c
+++ drivers/net/mlx5/linux/mlx5_os.c
@@ -1262,6 +1262,34 @@ err_secondary:
 	}
 	/* Override some values set by hardware configuration. */
 	mlx5_args(config, dpdk_dev->devargs);
+	/* Update final values for devargs before check sibling config. */
+#if !defined(HAVE_IBV_FLOW_DV_SUPPORT) || !defined(HAVE_MLX5DV_DR)
+	if (config->dv_flow_en) {
+		DRV_LOG(WARNING, "DV flow is not supported.");
+		config->dv_flow_en = 0;
+	}
+#endif
+#ifdef HAVE_MLX5DV_DR_ESWITCH
+	if (!(sh->cdev->config.hca_attr.eswitch_manager && config->dv_flow_en &&
+	      (switch_info->representor || switch_info->master)))
+		config->dv_esw_en = 0;
+#else
+	config->dv_esw_en = 0;
+#endif
+	if (config->dv_esw_en == 0) {
+		DRV_LOG(INFO, "Eswitch flows (transfer) are not supported.");
+		if (sh->cdev->config.hca_attr.eswitch_manager == 0)
+			DRV_LOG(INFO,
+				"Maybe cap_sys_rawio capability is not set?");
+	}
+	if (!priv->config.dv_esw_en &&
+	    priv->config.dv_xmeta_en != MLX5_XMETA_MODE_LEGACY) {
+		DRV_LOG(WARNING,
+			"Metadata mode %u is not supported (no E-Switch).",
+			priv->config.dv_xmeta_en);
+		priv->config.dv_xmeta_en = MLX5_XMETA_MODE_LEGACY;
+	}
+	/* Check sibling device configurations. */
 	err = mlx5_dev_check_sibling_config(priv, config, dpdk_dev);
 	if (err)
 		goto error;
@@ -1272,12 +1300,6 @@ err_secondary:
 #if !defined(HAVE_IBV_DEVICE_COUNTERS_SET_V42) && \
 	!defined(HAVE_IBV_DEVICE_COUNTERS_SET_V45)
 	DRV_LOG(DEBUG, "counters are not supported");
-#endif
-#if !defined(HAVE_IBV_FLOW_DV_SUPPORT) || !defined(HAVE_MLX5DV_DR)
-	if (config->dv_flow_en) {
-		DRV_LOG(WARNING, "DV flow is not supported");
-		config->dv_flow_en = 0;
-	}
 #endif
 	config->ind_table_max_size =
 		sh->device_attr.max_rwq_indirection_table_size;
@@ -1669,19 +1691,6 @@ err_secondary:
 	 * Verbs context returned by ibv_open_device().
 	 */
 	mlx5_link_update(eth_dev, 0);
-#ifdef HAVE_MLX5DV_DR_ESWITCH
-	if (!(config->hca_attr.eswitch_manager && config->dv_flow_en &&
-	      (switch_info->representor || switch_info->master)))
-		config->dv_esw_en = 0;
-#else
-	config->dv_esw_en = 0;
-#endif
-	if (config->dv_esw_en == 0) {
-		DRV_LOG(INFO, "Eswitch flows (transfer) are not supported.");
-		if (config->hca_attr.eswitch_manager == 0)
-			DRV_LOG(INFO,
-				"Maybe cap_sys_rawio capability is not set?");
-	}
 	/* Detect minimal data bytes to inline. */
 	mlx5_set_min_inline(spawn, config);
 	/* Store device configuration on private structure. */
