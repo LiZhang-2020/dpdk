LiZhang-2020/dpdk

commit 3209d52f7a1e243217def52b93d59c347e0f1188

net/mlx5: add port to metadata conversion
This patch initial version of functions used to:

- convert between ethdev port_id and internal tag/mask value,
- convert between IB context and internal tag/mask value.

Signed-off-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

drivers/net/mlx5/mlx5_flow.h
@@ -1354,6 +1354,50 @@ struct mlx5_flow_split_info {
 	uint64_t prefix_layers; /**< Prefix subflow layers. */
 };
 
+struct flow_hw_port_info {
+	uint32_t regc_mask;
+	uint32_t regc_value;
+};
+
+extern struct flow_hw_port_info mlx5_flow_hw_port_infos[RTE_MAX_ETHPORTS];
+
+/*
+ * Get metadata match tag and mask for given rte_eth_dev port.
+ * Used in HWS rule creation.
+ */
+static __rte_always_inline const struct flow_hw_port_info *
+flow_hw_conv_port_id(const uint16_t port_id)
+{
+	struct flow_hw_port_info *port_info;
+
+	if (port_id >= RTE_MAX_ETHPORTS)
+		return NULL;
+	port_info = &mlx5_flow_hw_port_infos[port_id];
+	return !!port_info->regc_mask ? port_info : NULL;
+}
+
+/*
+ * Get metadata match tag and mask for the uplink port represented
+ * by given IB context. Used in HWS context creation.
+ */
+static __rte_always_inline const struct flow_hw_port_info *
+flow_hw_get_wire_port(struct ibv_context *ibctx)
+{
+	uint16_t port_id;
+
+	MLX5_ETH_FOREACH_DEV(port_id, NULL) {
+		const struct mlx5_priv *priv =
+				rte_eth_devices[port_id].data->dev_private;
+
+		if (priv && priv->master && priv->sh->cdev->ctx == ibctx)
+			return flow_hw_conv_port_id(port_id);
+	}
+	return NULL;
+}
+
+void flow_hw_set_port_info(struct rte_eth_dev *dev);
+void flow_hw_clear_port_info(struct rte_eth_dev *dev);
+
 typedef int (*mlx5_flow_validate_t)(struct rte_eth_dev *dev,
 				    const struct rte_flow_attr *attr,
 				    const struct rte_flow_item items[],

drivers/net/mlx5/linux/mlx5_os.c
@@ -1739,6 +1739,8 @@ err_secondary:
 	priv->drop_queue.hrxq = mlx5_drop_action_create(eth_dev);
 	if (!priv->drop_queue.hrxq)
 		goto error;
+	if (priv->vport_meta_mask)
+		flow_hw_set_port_info(eth_dev);
 	if (priv->config.dv_flow_en == 2)
 		return eth_dev;
 	/* Port representor shares the same max prioirity with master. */

drivers/net/mlx5/mlx5.c
@@ -1601,6 +1601,7 @@ mlx5_dev_close(struct rte_eth_dev *dev)
 	mlx5_flex_parser_ecpri_release(dev);
 	mlx5_flex_item_port_cleanup(dev);
 	flow_hw_resource_release(dev);
+	flow_hw_clear_port_info(dev);
 	if (priv->rxq_privs != NULL) {
 		/* XXX race condition if mlx5_rx_burst() is still running. */
 		usleep(1000);

drivers/net/mlx5/mlx5_flow.c
@@ -36,6 +36,12 @@
 #include "mlx5_common_os.h"
 #include "rte_pmd_mlx5.h"
 
+/*
+ * Shared array for quick translation between port_id and vport mask/values
+ * used for HWS rules.
+ */
+struct flow_hw_port_info mlx5_flow_hw_port_infos[RTE_MAX_ETHPORTS];
+
 struct tunnel_default_miss_ctx {
 	uint16_t *queue;
 	__extension__

drivers/net/mlx5/mlx5_flow_hw.c
@@ -1936,6 +1936,33 @@ flow_hw_resource_release(struct rte_eth_dev *dev)
 	claim_zero(mlx5dr_context_close(priv->dr_ctx));
 }
 
+/* Sets vport tag and mask, for given port, used in HWS rules. */
+void
+flow_hw_set_port_info(struct rte_eth_dev *dev)
+{
+	struct mlx5_priv *priv = dev->data->dev_private;
+	uint16_t port_id = dev->data->port_id;
+	struct flow_hw_port_info *info;
+
+	MLX5_ASSERT(port_id < RTE_MAX_ETHPORTS);
+	info = &mlx5_flow_hw_port_infos[port_id];
+	info->regc_mask = priv->vport_meta_mask;
+	info->regc_value = priv->vport_meta_tag;
+}
+
+/* Clears vport tag and mask used for HWS rules. */
+void
+flow_hw_clear_port_info(struct rte_eth_dev *dev)
+{
+	uint16_t port_id = dev->data->port_id;
+	struct flow_hw_port_info *info;
+
+	MLX5_ASSERT(port_id < RTE_MAX_ETHPORTS);
+	info = &mlx5_flow_hw_port_infos[port_id];
+	info->regc_mask = 0;
+	info->regc_value = 0;
+}
+
 static struct rte_flow_action_handle *
 flow_hw_action_handle_create(struct rte_eth_dev *dev, uint32_t queue,
 			     const struct rte_flow_q_ops_attr *attr,

commit 4619985a987ccb31e1460bd6d4e2c5aae4b50605

net/mlx5: fix port conversion for wire
This patch fixes flow_hw_get_wire_port() function to find wire port
using IB context's device instead of IB context itself.

Fixes: 3209d52f7a1e ("net/mlx5: add port to metadata conversion")

Signed-off-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

drivers/net/mlx5/mlx5_flow.h
@@ -1383,14 +1383,19 @@ flow_hw_conv_port_id(const uint16_t port_id)
 static __rte_always_inline const struct flow_hw_port_info *
 flow_hw_get_wire_port(struct ibv_context *ibctx)
 {
+	struct ibv_device *ibdev = ibctx->device;
 	uint16_t port_id;
 
 	MLX5_ETH_FOREACH_DEV(port_id, NULL) {
 		const struct mlx5_priv *priv =
 				rte_eth_devices[port_id].data->dev_private;
 
-		if (priv && priv->master && priv->sh->cdev->ctx == ibctx)
-			return flow_hw_conv_port_id(port_id);
+		if (priv && priv->master) {
+			struct ibv_context *port_ibctx = priv->sh->cdev->ctx;
+
+			if (port_ibctx->device == ibdev)
+				return flow_hw_conv_port_id(port_id);
+		}
 	}
 	return NULL;
 }