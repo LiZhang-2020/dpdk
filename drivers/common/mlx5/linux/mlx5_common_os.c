LiZhang-2020/dpdk

commit 91cb8999fb50444c87c26e0e072d9922323d2c9d

common/mlx5: share interrupt management
There are many duplicate code of creating and initializing rte_intr_handle.
Add new mlx5_os APIs to do this, replace all PMD related code with this
API.

Signed-off-by: Spike Du <spiked@nvidia.com>

drivers/common/mlx5/linux/mlx5_common_os.c
@@ -11,6 +11,7 @@
 #endif
 #include <dirent.h>
 #include <net/if.h>
+#include <fcntl.h>
 
 #include <rte_errno.h>
 #include <rte_string_fns.h>
@@ -946,4 +947,127 @@ mlx5_os_wrapped_mkey_destroy(struct mlx5_pmd_wrapped_mr *pmd_mr)
 	if (pmd_mr->obj)
 		claim_zero(mlx5_glue->dereg_mr(pmd_mr->obj));
 	memset(pmd_mr, 0, sizeof(*pmd_mr));
+}
+
+/**
+ * Rte_intr_handle setup helper.
+ *
+ * @param[in] intr_handle
+ *   interrupt handle to be initialized.
+ * @param[in] set_fd_nonblock
+ *   Whether to set fd to O_NONBLOCK.
+ * @param[in] fd
+ *   Fd to set in created intr_handle.
+ * @param[in] cb
+ *   Callback to register for intr_handle.
+ * @param[in] cb_arg
+ *   Callback argument for cb.
+ *
+ * @return
+ *  - 0 on success.
+ *  - < 0 on failure, with rte_errno set.
+ */
+int
+mlx5_os_interrupt_handler_setup(struct rte_intr_handle *intr_handle,
+				bool set_fd_nonblock, int fd,
+				rte_intr_callback_fn cb, void *cb_arg)
+{
+	int ret, flags;
+
+	if (!intr_handle) {
+		rte_errno = EINVAL;
+		return -rte_errno;
+	}
+	memset(intr_handle, 0, sizeof(*intr_handle));
+	if (set_fd_nonblock) {
+		flags = fcntl(fd, F_GETFL);
+		ret = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+		if (ret) {
+			rte_errno = errno;
+			return -rte_errno;
+		}
+	}
+	intr_handle->fd = fd;
+	intr_handle->type = RTE_INTR_HANDLE_EXT;
+	ret = rte_intr_callback_register(intr_handle, cb, cb_arg);
+	if (ret) {
+		rte_errno = -ret;
+		return ret;
+	}
+	return 0;
+}
+
+/* Safe unregistration for interrupt callback. */
+static void
+mlx5_intr_callback_unregister(const struct rte_intr_handle *handle,
+			      rte_intr_callback_fn cb_fn, void *cb_arg)
+{
+	uint64_t twait = 0;
+	uint64_t start = 0;
+
+	do {
+		int ret;
+
+		ret = rte_intr_callback_unregister(handle, cb_fn, cb_arg);
+		if (ret >= 0)
+			return;
+		if (ret != -EAGAIN) {
+			DRV_LOG(INFO, "failed to unregister interrupt"
+				      " handler (error: %d)", ret);
+			MLX5_ASSERT(false);
+			return;
+		}
+		if (twait) {
+			struct timespec onems;
+
+			/* Wait one millisecond and try again. */
+			onems.tv_sec = 0;
+			onems.tv_nsec = NS_PER_S / MS_PER_S;
+			nanosleep(&onems, 0);
+			/* Check whether one second elapsed. */
+			if ((rte_get_timer_cycles() - start) <= twait)
+				continue;
+		} else {
+			/*
+			 * We get the amount of timer ticks for one second.
+			 * If this amount elapsed it means we spent one
+			 * second in waiting. This branch is executed once
+			 * on first iteration.
+			 */
+			twait = rte_get_timer_hz();
+			MLX5_ASSERT(twait);
+		}
+		/*
+		 * Timeout elapsed, show message (once a second) and retry.
+		 * We have no other acceptable option here, if we ignore
+		 * the unregistering return code the handler will not
+		 * be unregistered, fd will be closed and we may get the
+		 * crush. Hanging and messaging in the loop seems not to be
+		 * the worst choice.
+		 */
+		DRV_LOG(INFO, "Retrying to unregister interrupt handler");
+		start = rte_get_timer_cycles();
+	} while (true);
+}
+
+/**
+ * Rte_intr_handle unsetup helper.
+ *
+ * @param[in] intr_handle
+ *   Rte_intr_handle to destroy.
+ * @param[in] cb
+ *   Callback which is registered to intr_handle.
+ * @param[in] cb_arg
+ *   Callback argument for cb.
+ *
+ */
+void
+mlx5_os_interrupt_handler_unset(struct rte_intr_handle *intr_handle,
+				rte_intr_callback_fn cb, void *cb_arg)
+{
+	if (!intr_handle)
+		return;
+	if (intr_handle->fd >= 0)
+		mlx5_intr_callback_unregister(intr_handle, cb, cb_arg);
+	intr_handle->fd = -1;
 }

drivers/net/mlx5/linux/mlx5_os.c
@@ -2519,48 +2519,34 @@ mlx5_os_net_cleanup(void)
 void
 mlx5_os_dev_shared_handler_install(struct mlx5_dev_ctx_shared *sh)
 {
-	int ret;
-	int flags;
 	struct ibv_context *ctx = sh->cdev->ctx;
+	int ret;
 
-	sh->intr_handle.fd = -1;
-	flags = fcntl(ctx->async_fd, F_GETFL);
-	ret = fcntl(ctx->async_fd, F_SETFL, flags | O_NONBLOCK);
+	ret = mlx5_os_interrupt_handler_setup
+		(&sh->intr_handle, true,
+		 ctx->async_fd, mlx5_dev_interrupt_handler, sh);
 	if (ret) {
-		DRV_LOG(INFO, "failed to change file descriptor async event"
-			" queue");
-	} else {
-		sh->intr_handle.fd = ctx->async_fd;
-		sh->intr_handle.type = RTE_INTR_HANDLE_EXT;
-		if (rte_intr_callback_register(&sh->intr_handle,
-					mlx5_dev_interrupt_handler, sh)) {
-			DRV_LOG(INFO, "Fail to install the shared interrupt.");
-			sh->intr_handle.fd = -1;
-		}
+		DRV_LOG(ERR, "Failed to initialize intr_handle.");
+		return;
 	}
 	if (sh->cdev->config.devx) {
 #ifdef HAVE_IBV_DEVX_ASYNC
-		sh->intr_handle_devx.fd = -1;
+		struct mlx5dv_devx_cmd_comp *devx_comp;
+
 		sh->devx_comp = (void *)mlx5_glue->devx_create_cmd_comp(ctx);
-		struct mlx5dv_devx_cmd_comp *devx_comp = sh->devx_comp;
+		devx_comp = sh->devx_comp;
 		if (!devx_comp) {
 			DRV_LOG(INFO, "failed to allocate devx_comp.");
 			return;
 		}
-		flags = fcntl(devx_comp->fd, F_GETFL);
-		ret = fcntl(devx_comp->fd, F_SETFL, flags | O_NONBLOCK);
+		ret = mlx5_os_interrupt_handler_setup
+			(&sh->intr_handle_devx,
+			 true, devx_comp->fd,
+			 mlx5_dev_interrupt_handler_devx, sh);
 		if (ret) {
-			DRV_LOG(INFO, "failed to change file descriptor"
-				" devx comp");
-			return;
-		}
-		sh->intr_handle_devx.fd = devx_comp->fd;
-		sh->intr_handle_devx.type = RTE_INTR_HANDLE_EXT;
-		if (rte_intr_callback_register(&sh->intr_handle_devx,
-					mlx5_dev_interrupt_handler_devx, sh)) {
-			DRV_LOG(INFO, "Fail to install the devx shared"
+			DRV_LOG(ERR, "Failed to install the devx shared"
 				" interrupt.");
-			sh->intr_handle_devx.fd = -1;
+			return;
 		}
 #endif /* HAVE_IBV_DEVX_ASYNC */
 	}
@@ -2577,13 +2563,11 @@ mlx5_os_dev_shared_handler_install(struct mlx5_dev_ctx_shared *sh)
 void
 mlx5_os_dev_shared_handler_uninstall(struct mlx5_dev_ctx_shared *sh)
 {
-	if (sh->intr_handle.fd >= 0)
-		mlx5_intr_callback_unregister(&sh->intr_handle,
-					      mlx5_dev_interrupt_handler, sh);
+	mlx5_os_interrupt_handler_unset(&sh->intr_handle,
+					mlx5_dev_interrupt_handler, sh);
 #ifdef HAVE_IBV_DEVX_ASYNC
-	if (sh->intr_handle_devx.fd >= 0)
-		rte_intr_callback_unregister(&sh->intr_handle_devx,
-				  mlx5_dev_interrupt_handler_devx, sh);
+	mlx5_os_interrupt_handler_unset(&sh->intr_handle_devx,
+					mlx5_dev_interrupt_handler_devx, sh);
 	if (sh->devx_comp)
 		mlx5_glue->devx_destroy_cmd_comp(sh->devx_comp);
 #endif

drivers/vdpa/mlx5/mlx5_vdpa_virtq.c
@@ -92,20 +92,10 @@ mlx5_vdpa_virtqs_cleanup(struct mlx5_vdpa_priv *priv)
 static int
 mlx5_vdpa_virtq_unset(struct mlx5_vdpa_virtq *virtq)
 {
-	int ret = -EAGAIN;
+	int ret;
 
-	if (virtq->intr_handle.fd >= 0) {
-		while (ret == -EAGAIN) {
-			ret = rte_intr_callback_unregister(&virtq->intr_handle,
+	mlx5_os_interrupt_handler_unset(&virtq->intr_handle,
 					mlx5_vdpa_virtq_kick_handler, virtq);
-			if (ret == -EAGAIN) {
-				DRV_LOG(DEBUG, "Try again to unregister fd %d of virtq %hu interrupt",
-					virtq->intr_handle.fd, virtq->index);
-				usleep(MLX5_VDPA_INTR_RETRIES_USEC);
-			}
-		}
-		virtq->intr_handle.fd = -1;
-	}
 	if (virtq->virtq) {
 		ret = mlx5_vdpa_virtq_stop(virtq->priv, virtq->index);
 		if (ret)
@@ -360,21 +350,13 @@ reuse:
 	virtq->priv = priv;
 	rte_write32(virtq->index, priv->virtq_db_addr);
 	/* Setup doorbell mapping. */
-	virtq->intr_handle.fd = vq.kickfd;
-	if (virtq->intr_handle.fd < 0) {
-		DRV_LOG(WARNING, "Virtq %d kickfd is invalid.", index);
-	} else {
-		virtq->intr_handle.type = RTE_INTR_HANDLE_EXT;
-		if (rte_intr_callback_register(&virtq->intr_handle,
-				mlx5_vdpa_virtq_kick_handler, virtq)) {
-			virtq->intr_handle.fd = -1;
-			DRV_LOG(ERR, "Failed to register virtq %d interrupt.",
-				index);
-			goto error;
-		} else {
-			DRV_LOG(DEBUG, "Register fd %d interrupt for virtq %d.",
-				virtq->intr_handle.fd, index);
-		}
+	ret = mlx5_os_interrupt_handler_setup
+		(&virtq->intr_handle, false,
+		 vq.kickfd, mlx5_vdpa_virtq_kick_handler,
+		 virtq);
+	if (ret) {
+		DRV_LOG(ERR, "Fail to initialize virtrq intr_handle");
+		goto error;
 	}
 	/* Subscribe virtq error event. */
 	virtq->version++;

drivers/common/mlx5/linux/mlx5_common_os.h
@@ -14,6 +14,7 @@
 #include <rte_log.h>
 #include <rte_kvargs.h>
 #include <rte_devargs.h>
+#include <rte_interrupts.h>
 
 #include "mlx5_autoconf.h"
 #include "mlx5_glue.h"
@@ -224,5 +225,16 @@ mlx5_get_device_guid(const struct rte_pci_addr *dev, uint8_t *guid, size_t len);
 
 void
 mlx5_set_context_attr(struct rte_device *dev, struct ibv_context *ctx);
+
+__rte_internal
+int
+mlx5_os_interrupt_handler_setup(struct rte_intr_handle *intr_handle,
+				 bool set_fd_nonblock, int fd,
+				 rte_intr_callback_fn cb, void *cb_arg);
+
+__rte_internal
+void
+mlx5_os_interrupt_handler_unset(struct rte_intr_handle *intr_handle,
+				  rte_intr_callback_fn cb, void *cb_arg);
 
 #endif /* RTE_PMD_MLX5_COMMON_OS_H_ */

drivers/common/mlx5/version.map
@@ -143,6 +143,7 @@ INTERNAL {
 	mlx5_mp_req_mempool_reg;
 	mlx5_mr_mempool2mr_bh;
 	mlx5_mr_mempool_populate_cache;
-
+	mlx5_os_interrupt_handler_setup;
+	mlx5_os_interrupt_handler_unset;
 	local: *;
 };

drivers/net/mlx5/linux/mlx5_ethdev_os.c
@@ -846,77 +846,6 @@ mlx5_dev_interrupt_handler(void *cb_arg)
 	}
 }
 
-/*
- * Unregister callback handler safely. The handler may be active
- * while we are trying to unregister it, in this case code -EAGAIN
- * is returned by rte_intr_callback_unregister(). This routine checks
- * the return code and tries to unregister handler again.
- *
- * @param handle
- *   interrupt handle
- * @param cb_fn
- *   pointer to callback routine
- * @cb_arg
- *   opaque callback parameter
- */
-void
-mlx5_intr_callback_unregister(const struct rte_intr_handle *handle,
-			      rte_intr_callback_fn cb_fn, void *cb_arg)
-{
-	/*
-	 * Try to reduce timeout management overhead by not calling
-	 * the timer related routines on the first iteration. If the
-	 * unregistering succeeds on first call there will be no
-	 * timer calls at all.
-	 */
-	uint64_t twait = 0;
-	uint64_t start = 0;
-
-	do {
-		int ret;
-
-		ret = rte_intr_callback_unregister(handle, cb_fn, cb_arg);
-		if (ret >= 0)
-			return;
-		if (ret != -EAGAIN) {
-			DRV_LOG(INFO, "failed to unregister interrupt"
-				      " handler (error: %d)", ret);
-			MLX5_ASSERT(false);
-			return;
-		}
-		if (twait) {
-			struct timespec onems;
-
-			/* Wait one millisecond and try again. */
-			onems.tv_sec = 0;
-			onems.tv_nsec = NS_PER_S / MS_PER_S;
-			nanosleep(&onems, 0);
-			/* Check whether one second elapsed. */
-			if ((rte_get_timer_cycles() - start) <= twait)
-				continue;
-		} else {
-			/*
-			 * We get the amount of timer ticks for one second.
-			 * If this amount elapsed it means we spent one
-			 * second in waiting. This branch is executed once
-			 * on first iteration.
-			 */
-			twait = rte_get_timer_hz();
-			MLX5_ASSERT(twait);
-		}
-		/*
-		 * Timeout elapsed, show message (once a second) and retry.
-		 * We have no other acceptable option here, if we ignore
-		 * the unregistering return code the handler will not
-		 * be unregistered, fd will be closed and we may get the
-		 * crush. Hanging and messaging in the loop seems not to be
-		 * the worst choice.
-		 */
-		DRV_LOG(INFO, "Retrying to unregister interrupt handler");
-		start = rte_get_timer_cycles();
-	} while (true);
-}
-
 /**
  * Handle DEVX interrupts from the NIC.
  * This function is probably called from the DPDK host thread.

drivers/net/mlx5/linux/mlx5_socket.c
@@ -130,39 +130,6 @@ error:
 }
 
 /**
- * Install interrupt handler.
- *
- * @param dev
- *   Pointer to Ethernet device.
- * @return
- *   0 on success, a negative errno value otherwise.
- */
-static int
-mlx5_pmd_interrupt_handler_install(void)
-{
-	MLX5_ASSERT(server_socket != -1);
-	server_intr_handle.fd = server_socket;
-	server_intr_handle.type = RTE_INTR_HANDLE_EXT;
-	return rte_intr_callback_register(&server_intr_handle,
-					  mlx5_pmd_socket_handle, NULL);
-}
-
-/**
- * Uninstall interrupt handler.
- */
-static void
-mlx5_pmd_interrupt_handler_uninstall(void)
-{
-	if (server_socket != -1) {
-		mlx5_intr_callback_unregister(&server_intr_handle,
-					      mlx5_pmd_socket_handle,
-					      NULL);
-	}
-	server_intr_handle.fd = 0;
-	server_intr_handle.type = RTE_INTR_HANDLE_UNKNOWN;
-}
-
-/**
  * Initialise the socket to communicate with external tools.
  *
  * @return
@@ -208,8 +175,12 @@ mlx5_pmd_socket_init(void)
 			strerror(errno));
 		goto remove;
 	}
-	if (mlx5_pmd_interrupt_handler_install()) {
-		DRV_LOG(WARNING, "cannot register interrupt handler for mlx5 socket: %s",
+	ret = mlx5_os_interrupt_handler_setup
+		(&server_intr_handle, false,
+		 server_socket, mlx5_pmd_socket_handle, NULL);
+	if (ret) {
+		DRV_LOG(WARNING, "cannot register interrupt handler for "
+			"mlx5 socket: %s",
 			strerror(errno));
 		goto remove;
 	}
@@ -232,7 +203,8 @@ mlx5_pmd_socket_uninit(void)
 {
 	if (server_socket == -1)
 		return;
-	mlx5_pmd_interrupt_handler_uninstall();
+	mlx5_os_interrupt_handler_unset(&server_intr_handle,
+					mlx5_pmd_socket_handle, NULL);
 	claim_zero(close(server_socket));
 	server_socket = -1;
 	MKSTR(path, MLX5_SOCKET_PATH, getpid());

drivers/net/mlx5/mlx5.h
@@ -1709,8 +1709,6 @@ int mlx5_sysfs_switch_info(unsigned int ifindex,
 			   struct mlx5_switch_info *info);
 void mlx5_translate_port_name(const char *port_name_in,
 			      struct mlx5_switch_info *port_info_out);
-void mlx5_intr_callback_unregister(const struct rte_intr_handle *handle,
-				   rte_intr_callback_fn cb_fn, void *cb_arg);
 int mlx5_sysfs_bond_info(unsigned int pf_ifindex, unsigned int *ifindex,
 			 char *ifname);
 int mlx5_get_module_info(struct rte_eth_dev *dev,

commit 88ba8f80c31be6507727106a32712d9089744c31

common/mlx5: fix interrupt handler unregisteration crash
when unsetup interrupt handler whose fd is 0 with rte assert enabled,
program crashes at mlx5_intr_callback_unregister.
rte_intr_handle's fd is normally 0 before setup correctly, when unsetup
interrupt handler, fd 0 is deemed as valid and tries to unregister it,
the rte_intr_callback_unregister returns -ENOENT in
mlx5_intr_callback_unregister, and causes assertion failure.
Initialize all interrupt handle fd to -1 so that we do nothing when
unset.

Fixes: 91cb8999fb50 ("common/mlx5: share interrupt management")

Signed-off-by: Spike Du <spiked@nvidia.com>

drivers/common/mlx5/linux/mlx5_common_os.c
@@ -974,11 +974,12 @@ mlx5_os_interrupt_handler_setup(struct rte_intr_handle *intr_handle,
 {
 	int ret, flags;
 
-	if (!intr_handle) {
+	if (!intr_handle || fd < 0) {
 		rte_errno = EINVAL;
 		return -rte_errno;
 	}
 	memset(intr_handle, 0, sizeof(*intr_handle));
+	intr_handle->fd = -1;
 	if (set_fd_nonblock) {
 		flags = fcntl(fd, F_GETFL);
 		ret = fcntl(fd, F_SETFL, flags | O_NONBLOCK);

drivers/net/mlx5/linux/mlx5_os.c
@@ -2550,10 +2550,8 @@ mlx5_os_dev_shared_handler_install(struct mlx5_dev_ctx_shared *sh)
 	ret = mlx5_os_interrupt_handler_setup
 		(&sh->intr_handle, true,
 		 ctx->async_fd, mlx5_dev_interrupt_handler, sh);
-	if (ret) {
-		DRV_LOG(ERR, "Failed to initialize intr_handle.");
-		return;
-	}
+	if (ret)
+		DRV_LOG(INFO, "Failed to initialize intr_handle.");
 	if (sh->cdev->config.devx) {
 #ifdef HAVE_IBV_DEVX_ASYNC
 		struct mlx5dv_devx_cmd_comp *devx_comp;
@@ -2568,11 +2566,9 @@ mlx5_os_dev_shared_handler_install(struct mlx5_dev_ctx_shared *sh)
 			(&sh->intr_handle_devx,
 			 true, devx_comp->fd,
 			 mlx5_dev_interrupt_handler_devx, sh);
-		if (ret) {
-			DRV_LOG(ERR, "Failed to install the devx shared"
+		if (ret)
+			DRV_LOG(INFO, "Failed to install the devx shared"
 				" interrupt.");
-			return;
-		}
 #endif /* HAVE_IBV_DEVX_ASYNC */
 	}
 }

drivers/net/mlx5/mlx5.c
@@ -1469,6 +1469,9 @@ mlx5_alloc_shared_dev_ctx(const struct mlx5_dev_spawn_data *spawn,
 		rte_errno = ENOMEM;
 		goto exit;
 	}
+	sh->intr_handle.fd = -1;
+	sh->intr_handle_devx.fd = -1;
+	sh->intr_handle_lwm.fd = -1;
 	pthread_mutex_init(&sh->txpp.mutex, NULL);
 	sh->numa_node = spawn->cdev->dev->numa_node;
 	sh->cdev = spawn->cdev;

drivers/vdpa/mlx5/mlx5_vdpa_virtq.c
@@ -527,6 +527,7 @@ mlx5_vdpa_virtq_setup(struct mlx5_vdpa_priv *priv, int index, bool reg_kick)
 	rte_spinlock_unlock(&priv->db_lock);
 	/* Setup doorbell mapping. */
 	if (reg_kick) {
+		virtq->intr_handle.fd = -1;
 		ret = mlx5_os_interrupt_handler_setup
 			(&virtq->intr_handle, false,
 			 vq.kickfd, mlx5_vdpa_virtq_kick_handler,