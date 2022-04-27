LiZhang-2020/dpdk

commit 22c2368bb85773f856b165e7ee00bf596a027002

net/mlx5: add HW steering counter service thread
Add main loop of background thread for counter query.
The counter query functionality is not included in this commit.

Signed-off-by: Xiaoyu Min <jackmin@nvidia.com>

drivers/net/mlx5/mlx5_hws_cnt.c
@@ -5,9 +5,72 @@
 #include <stdint.h>
 #include <rte_malloc.h>
 #include <mlx5_malloc.h>
+#include <rte_ring.h>
 
 #include "mlx5_utils.h"
 #include "mlx5_hws_cnt.h"
+
+static void
+__mlx5_hws_cnt_svc(struct mlx5_dev_ctx_shared *sh,
+		struct mlx5_hws_cnt_pool *cpool)
+{
+	struct rte_ring *reset_list = cpool->wait_reset_list;
+	struct rte_ring *free_list = cpool->free_list;
+	uint32_t reset_cnt_num;
+	struct rte_ring_zc_data zcdr = {0};
+	struct rte_ring_zc_data zcdf = {0};
+
+	reset_cnt_num = rte_ring_count(reset_list);
+	do {
+		cpool->query_gen++;
+		mlx5_hws_cnt_pool_aso_query(sh, cpool);
+		zcdr.n1 = 0;
+		zcdf.n1 = 0;
+		rte_ring_enqueue_zc_burst_elem_start(free_list,
+				sizeof(cnt_id_t), reset_cnt_num, &zcdf,
+				NULL);
+		rte_ring_dequeue_zc_burst_elem_start(reset_list,
+				sizeof(cnt_id_t), reset_cnt_num, &zcdr,
+				NULL);
+		__hws_cnt_r2rcpy(&zcdf, &zcdr, reset_cnt_num);
+		rte_ring_dequeue_zc_elem_finish(reset_list,
+				reset_cnt_num);
+		rte_ring_enqueue_zc_elem_finish(free_list,
+				reset_cnt_num);
+		reset_cnt_num = rte_ring_count(reset_list);
+	} while (reset_cnt_num > 0);
+}
+
+static void *
+mlx5_hws_cnt_svc(void *opaque)
+{
+	struct mlx5_dev_ctx_shared *sh =
+		(struct mlx5_dev_ctx_shared *)opaque;
+	uint32_t interval = sh->cnt_svc->query_interval;
+	uint16_t port_id;
+	uint64_t start_cycle, query_cycle = 0;
+	uint64_t query_us;
+	uint64_t sleep_us;
+
+	while (sh->cnt_svc->svc_running != 0) {
+		start_cycle = rte_rdtsc();
+		MLX5_ETH_FOREACH_DEV(port_id, sh->cdev->dev) {
+			struct mlx5_priv *opriv =
+				rte_eth_devices[port_id].data->dev_private;
+			if (opriv != NULL &&
+			    opriv->sh == sh &&
+			    opriv->hws_cpool != NULL) {
+				__mlx5_hws_cnt_svc(sh, opriv->hws_cpool);
+			}
+		}
+		query_cycle = rte_rdtsc() - start_cycle;
+		query_us = query_cycle / (rte_get_timer_hz() / US_PER_S);
+		sleep_us = interval - query_us;
+		if (interval > query_us)
+			rte_delay_us_sleep(sleep_us);
+	}
+	return NULL;
+}
 
 struct mlx5_hws_cnt_pool *
 mlx5_hws_cnt_pool_init(const struct mlx5_hws_cnt_pool_cfg *pcfg,
@@ -97,4 +160,49 @@ mlx5_hws_cnt_pool_deinit(struct mlx5_hws_cnt_pool * const cntp)
 	mlx5_free(cntp->raw_mng);
 	mlx5_free(cntp->pool);
 	mlx5_free(cntp);
+}
+
+int
+mlx5_hws_cnt_service_thread_create(struct mlx5_dev_ctx_shared *sh)
+{
+	char name[NAME_MAX];
+	cpu_set_t cpuset;
+	int ret;
+	uint32_t service_core = sh->cnt_svc->service_core;
+
+	CPU_ZERO(&cpuset);
+	sh->cnt_svc->svc_running = 1;
+	ret = pthread_create(&sh->cnt_svc->service_thread, NULL,
+			mlx5_hws_cnt_svc, sh);
+	if (ret != 0) {
+		DRV_LOG(ERR, "Failed to create HW steering's counter service thread.");
+		return -ENOSYS;
+	}
+	snprintf(name, NAME_MAX - 1, "%s/svc@%d",
+		 sh->ibdev_name, service_core);
+	rte_thread_setname(sh->cnt_svc->service_thread, name);
+	CPU_SET(service_core, &cpuset);
+	pthread_setaffinity_np(sh->cnt_svc->service_thread, sizeof(cpuset),
+				&cpuset);
+	return 0;
+}
+
+void
+mlx5_hws_cnt_service_thread_destroy(struct mlx5_dev_ctx_shared *sh)
+{
+	if (sh->cnt_svc->service_thread == 0)
+		return;
+	sh->cnt_svc->svc_running = 0;
+	pthread_join(sh->cnt_svc->service_thread, NULL);
+	sh->cnt_svc->service_thread = 0;
+}
+
+int
+mlx5_hws_cnt_pool_aso_query(struct mlx5_dev_ctx_shared *sh,
+			    struct mlx5_hws_cnt_pool *cpool)
+{
+	/* To be added later. */
+	RTE_SET_USED(sh);
+	RTE_SET_USED(cpool);
+	return 0;
 }

drivers/net/mlx5/mlx5.c
@@ -191,6 +191,12 @@
 /* Device parameter to create the fdb default rule in PMD */
 #define MLX5_FDB_DEFAULT_RULE_EN "fdb_def_rule_en"
 
+/* HW steering counter configuration. */
+#define MLX5_HWS_CNT_SERVICE_CORE "service_core"
+
+/* HW steering counter's query interval. */
+#define MLX5_HWS_CNT_CYCLE_TIME "svc_cycle_time"
+
 /* Shared memory between primary and secondary processes. */
 struct mlx5_shared_data *mlx5_shared_data;
 
@@ -1249,6 +1255,10 @@ mlx5_dev_args_check_handler(const char *key, const char *val, void *opaque)
 		config->allow_duplicate_pattern = !!tmp;
 	} else if (strcmp(MLX5_FDB_DEFAULT_RULE_EN, key) == 0) {
 		config->fdb_def_rule = !!tmp;
+	} else if (strcmp(MLX5_HWS_CNT_SERVICE_CORE, key) == 0) {
+		config->cnt_svc.service_core = tmp;
+	} else if (strcmp(MLX5_HWS_CNT_CYCLE_TIME, key) == 0) {
+		config->cnt_svc.cycle_time = tmp;
 	}
 	return 0;
 }
@@ -1288,6 +1298,8 @@ mlx5_shared_dev_ctx_args_config(struct mlx5_dev_ctx_shared *sh,
 		MLX5_DECAP_EN,
 		MLX5_ALLOW_DUPLICATE_PATTERN,
 		MLX5_FDB_DEFAULT_RULE_EN,
+		MLX5_HWS_CNT_SERVICE_CORE,
+		MLX5_HWS_CNT_CYCLE_TIME,
 		NULL,
 	};
 	int ret = 0;
@@ -1300,6 +1312,8 @@ mlx5_shared_dev_ctx_args_config(struct mlx5_dev_ctx_shared *sh,
 	config->decap_en = 1;
 	config->allow_duplicate_pattern = 1;
 	config->fdb_def_rule = 1;
+	config->cnt_svc.cycle_time = MLX5_CNT_SVC_CYCLE_TIME_DEFAULT;
+	config->cnt_svc.service_core = rte_get_main_lcore();
 	if (mkvlist != NULL) {
 		/* Process parameters. */
 		ret = mlx5_kvargs_process(mkvlist, params,

drivers/net/mlx5/mlx5.h
@@ -311,6 +311,10 @@ struct mlx5_sh_config {
 	uint32_t decap_en:1; /* Whether decap will be used or not. */
 	uint32_t hw_fcs_strip:1; /* FCS stripping is supported. */
 	uint32_t allow_duplicate_pattern:1;
+	struct {
+		uint16_t service_core;
+		uint32_t cycle_time; /* query cycle time in milli-second. */
+	} cnt_svc; /* configure for HW steering's counter's service. */
 	/* Allow/Prevent the duplicate rules pattern. */
 	uint32_t fdb_def_rule:1; /* Create FDB default jump rule */
 };
@@ -1232,6 +1236,20 @@ struct mlx5_flex_item {
 	struct mlx5_flex_pattern_field map[MLX5_FLEX_ITEM_MAPPING_NUM];
 };
 
+struct mlx5_hws_aso_mng {
+	uint16_t sq_num;
+	struct mlx5_aso_sq *sqs;
+};
+
+struct mlx5_hws_cnt_svc_mng {
+	uint32_t refcnt;
+	uint32_t service_core;
+	uint32_t query_interval;
+	pthread_t service_thread;
+	uint8_t svc_running;
+	struct mlx5_hws_aso_mng aso_mng __rte_cache_aligned;
+};
+
 /*
  * Shared Infiniband device context for Master/Representors
  * which belong to same IB device with multiple IB ports.
@@ -1339,6 +1357,7 @@ struct mlx5_dev_ctx_shared {
 	uint32_t host_shaper_rate:8;
 	uint32_t lwm_triggered:1;
 	void *hws_tx;
+	struct mlx5_hws_cnt_svc_mng *cnt_svc;
 	struct mlx5_dev_shared_port port[]; /* per device port data array. */
 };
 
@@ -1650,6 +1669,7 @@ struct mlx5_priv {
 	LIST_HEAD(flow_hw_tbl, rte_flow_template_table) flow_hw_tbl;
 	/**< HW steering rte flow table list header. */
 	struct mlx5_indexed_pool *acts_ipool; /* Action data indexed pool. */
+	struct mlx5_hws_cnt_pool *hws_cpool; /* HW steering's counter pool. */
 };
 
 #define PORT_ID(priv) ((priv)->dev_data->port_id)

drivers/net/mlx5/mlx5_defs.h
@@ -185,4 +185,6 @@
 #define static_assert _Static_assert
 #endif
 
+#define MLX5_CNT_SVC_CYCLE_TIME_DEFAULT 500
+
 #endif /* RTE_PMD_MLX5_DEFS_H_ */

drivers/net/mlx5/mlx5_hws_cnt.h
@@ -26,11 +26,6 @@ typedef uint32_t cnt_id_t;
 
 #define MLX5_HWS_CNT_DCS_NUM 4
 
-struct mlx5_hws_aso_mng {
-	uint16_t sq_num;
-	struct mlx5_aso_sq *sqs;
-};
-
 struct mlx5_hws_cnt_dcs {
 	void *dr_action;
 	uint32_t batch_sz;
@@ -84,12 +79,6 @@ struct mlx5_hws_cnt_pool_caches {
 	uint32_t preload_sz;
 	uint32_t q_num;
 	struct rte_ring *qcache[];
-};
-
-struct mlx5_hws_cnt_svc {
-	uint32_t service_core;
-	uint32_t query_interval;
-	struct mlx5_hws_aso_mng aso_mng __rte_cache_aligned;
 };
 
 struct mlx5_hws_cnt_pool {
@@ -362,4 +351,15 @@ mlx5_hws_cnt_pool_init(const struct mlx5_hws_cnt_pool_cfg *pcfg,
 
 void
 mlx5_hws_cnt_pool_deinit(struct mlx5_hws_cnt_pool *cntp);
+
+int
+mlx5_hws_cnt_service_thread_create(struct mlx5_dev_ctx_shared *sh);
+
+void
+mlx5_hws_cnt_service_thread_destroy(struct mlx5_dev_ctx_shared *sh);
+
+int
+mlx5_hws_cnt_pool_aso_query(struct mlx5_dev_ctx_shared *sh,
+			    struct mlx5_hws_cnt_pool *cpool);
+
 #endif /* _MLX5_HWS_CNT_H_ */

commit 7628b7215f8949d47efbae4a7652fcfe8f40ed2d

net/mlx5: fix sleep time in hws counter query
The interval time's unit is mS but in query thread it is wronly taken as
uS which cause the sleep time is always zero and lead to query in a busy
loop.

Fix above by setting to correct value in unit uS.

Fixes: 22c2368bb857 ("net/mlx5: add HW steering counter service thread")
Signed-off-by: Xiaoyu Min <jackmin@nvidia.com>

drivers/net/mlx5/mlx5_hws_cnt.c
@@ -122,7 +122,8 @@ mlx5_hws_cnt_svc(void *opaque)
 {
 	struct mlx5_dev_ctx_shared *sh =
 		(struct mlx5_dev_ctx_shared *)opaque;
-	uint32_t interval = sh->cnt_svc->query_interval;
+	uint64_t interval =
+		(uint64_t)sh->cnt_svc->query_interval * (US_PER_S / MS_PER_S);
 	uint16_t port_id;
 	uint64_t start_cycle, query_cycle = 0;
 	uint64_t query_us;