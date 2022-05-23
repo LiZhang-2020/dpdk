LiZhang-2020/dpdk

commit 53ce799e73b4e19cbabaf5c40f4e30b155ea3c64

net/mlx5: backport from upstream for single flow dump
Backport changes for upstream.
Modify API mlx5_flow_dev_dump to support the feature.
Modify mlx5_socket since one extra arg flow_ptr is added.

The data structure sent to DPDK application from the utility triggering
the flow dumps should be packed and endianness must be specified.
The native host endianness can be used, all exchange happens within
the same host (we use sendmsg aux data and share the file handle,
remote approach is not applicable, no inter-host communication happens).

The message structure to dump one/all flow(s):
struct mlx5_flow_dump_req {
	uint32_t port_id;
		uint64_t flow_ptr;
	} __rte_packed;

If flow_ptr is 0, all flows for the specified port will be dumped.

Signed-off-by: Haifei Luo <haifeil@nvidia.com>

drivers/net/mlx5/linux/mlx5_socket.c
@@ -32,28 +32,29 @@ static void
 mlx5_pmd_socket_handle(void *cb __rte_unused)
 {
 	int conn_sock;
-	int ret, j;
+	int ret;
 	struct cmsghdr *cmsg = NULL;
-	#define LENGTH 9
-	/*The first byte for port_id and the rest for flowptr.*/
-	int data[LENGTH];
+	uint32_t data[MLX5_SENDMSG_MAX / sizeof(uint32_t)];
 	uint64_t flow_ptr = 0;
-	char buf[CMSG_SPACE(sizeof(int))] = { 0 };
+	uint8_t  buf[CMSG_SPACE(sizeof(int))] = { 0 };
 	struct iovec io = {
-		.iov_base = &data[0],
+		.iov_base = data,
 		.iov_len = sizeof(data),
-	};
+		};
 	struct msghdr msg = {
 		.msg_iov = &io,
 		.msg_iovlen = 1,
 		.msg_control = buf,
 		.msg_controllen = sizeof(buf),
 	};
-	uint16_t port_id;
+
+	uint32_t port_id;
 	int fd;
 	FILE *file = NULL;
 	struct rte_eth_dev *dev;
 	struct rte_flow_error err;
+	struct mlx5_flow_dump_req  *dump_req;
+	struct mlx5_flow_dump_ack  *dump_ack;
 
 	memset(data, 0, sizeof(data));
 	/* Accept the connection from the client. */
@@ -63,15 +64,16 @@ mlx5_pmd_socket_handle(void *cb __rte_unused)
 		return;
 	}
 	ret = recvmsg(conn_sock, &msg, MSG_WAITALL);
-	if (ret < 0) {
+	if (ret != sizeof(struct mlx5_flow_dump_req)) {
 		DRV_LOG(WARNING, "wrong message received: %s",
 			strerror(errno));
 		goto error;
 	}
+
 	/* Receive file descriptor. */
 	cmsg = CMSG_FIRSTHDR(&msg);
 	if (cmsg == NULL || cmsg->cmsg_type != SCM_RIGHTS ||
-	    cmsg->cmsg_len < sizeof(int)) {
+	cmsg->cmsg_len < sizeof(int)) {
 		DRV_LOG(WARNING, "invalid file descriptor message");
 		goto error;
 	}
@@ -86,20 +88,27 @@ mlx5_pmd_socket_handle(void *cb __rte_unused)
 		DRV_LOG(WARNING, "wrong port number message");
 		goto error;
 	}
-	memcpy(&port_id, msg.msg_iov->iov_base, sizeof(port_id));
+
+	dump_req = (struct mlx5_flow_dump_req *)msg.msg_iov->iov_base;
+	if (dump_req) {
+		port_id = dump_req->port_id;
+		flow_ptr = dump_req->flow_id;
+	} else {
+		DRV_LOG(WARNING, "Invalid message");
+		goto error;
+	}
+
 	if (!rte_eth_dev_is_valid_port(port_id)) {
 		DRV_LOG(WARNING, "Invalid port %u", port_id);
 		goto error;
 	}
+
 	/* Dump flow. */
 	dev = &rte_eth_devices[port_id];
-	/*The first byte in data for port_id and the following 8 for flowptr*/
-	for (j = 1; j < LENGTH; j++)
-		flow_ptr = (flow_ptr << 8) + data[j];
 	if (flow_ptr == 0)
-		ret = mlx5_flow_dev_dump(dev, file, NULL);
+		ret = mlx5_flow_dev_dump(dev, NULL, file, NULL);
 	else
-		ret = mlx5_flow_dump_rule(dev,
+		ret = mlx5_flow_dev_dump(dev,
 			(struct rte_flow *)((uintptr_t)flow_ptr), file, &err);
 
 	/* Set-up the ancillary data and reply. */
@@ -107,9 +116,10 @@ mlx5_pmd_socket_handle(void *cb __rte_unused)
 	msg.msg_control = NULL;
 	msg.msg_iovlen = 1;
 	msg.msg_iov = &io;
-	data[0] = -ret;
-	io.iov_len = sizeof(data[0]);
-	io.iov_base = &data[0];
+	dump_ack = (struct mlx5_flow_dump_ack *)data;
+	dump_ack->rc = -ret;
+	io.iov_len = sizeof(struct mlx5_flow_dump_ack);
+	io.iov_base = dump_ack;
 	do {
 		ret = sendmsg(conn_sock, &msg, 0);
 	} while (ret < 0 && errno == EINTR);

drivers/net/mlx5/linux/mlx5_os.h
@@ -14,6 +14,9 @@ enum {
 	DEV_SYSFS_PATH_MAX = IBV_SYSFS_PATH_MAX + 1
 };
 
+/* Maximal data of sendmsg message(in bytes). */
+#define MLX5_SENDMSG_MAX 64
+
 #define PCI_DRV_FLAGS  (RTE_PCI_DRV_INTR_LSC | \
 			RTE_PCI_DRV_INTR_RMV | \
 			RTE_PCI_DRV_PROBE_AGAIN)

drivers/net/mlx5/mlx5.h
@@ -114,6 +114,16 @@ struct mlx5_dev_spawn_data {
 	struct mlx5_bond_info *bond_info;
 };
 
+/** Data associated with socket messages. */
+struct mlx5_flow_dump_req  {
+	uint32_t port_id;
+	uint64_t flow_id;
+} __rte_packed;
+
+struct mlx5_flow_dump_ack {
+	int rc; /**< Return code. */
+};
+
 /** Key string for IPC. */
 #define MLX5_MP_NAME "net_mlx5_mp"
 
@@ -1593,11 +1603,8 @@ uint32_t mlx5_counter_alloc(struct rte_eth_dev *dev);
 void mlx5_counter_free(struct rte_eth_dev *dev, uint32_t cnt);
 int mlx5_counter_query(struct rte_eth_dev *dev, uint32_t cnt,
 		       bool clear, uint64_t *pkts, uint64_t *bytes);
-int mlx5_flow_dev_dump(struct rte_eth_dev *dev, FILE *file,
-		       struct rte_flow_error *error);
-int mlx5_flow_dump_rule(struct rte_eth_dev *dev, struct rte_flow *flow,
-			FILE *file,
-			struct rte_flow_error *error);
+int mlx5_flow_dev_dump(struct rte_eth_dev *dev, struct rte_flow *flow,
+			FILE *file, struct rte_flow_error *error);
 void mlx5_flow_rxq_dynf_metadata_set(struct rte_eth_dev *dev);
 int mlx5_flow_get_aged_flows(struct rte_eth_dev *dev, void **contexts,
 			uint32_t nb_contexts, struct rte_flow_error *error);

drivers/net/mlx5/mlx5_flow.c
@@ -650,7 +650,6 @@ static const struct rte_flow_ops mlx5_flow_ops = {
 	.isolate = mlx5_flow_isolate,
 	.query = mlx5_flow_query,
 	.dev_dump = mlx5_flow_dev_dump,
-	.dev_single_dump = mlx5_flow_dump_rule,
 	.get_aged_flows = mlx5_flow_get_aged_flows,
 	.shared_action_create = mlx5_shared_action_create,
 	.shared_action_destroy = mlx5_shared_action_destroy,
@@ -7637,6 +7636,8 @@ mlx5_flow_discover_mreg_c(struct rte_eth_dev *dev)
  *
  * @param[in] dev
  *    The pointer to Ethernet device.
+ * @param[in] flow_idx
+ *    The pointer to flow rule.
  * @param[in] file
  *   A pointer to a file for output.
  * @param[out] error
@@ -7646,33 +7647,16 @@ mlx5_flow_discover_mreg_c(struct rte_eth_dev *dev)
  *   0 on success, a nagative value otherwise.
  */
 int
-mlx5_flow_dev_dump(struct rte_eth_dev *dev,
-		   FILE *file,
-		   struct rte_flow_error *error __rte_unused)
-{
-	struct mlx5_priv *priv = dev->data->dev_private;
-	struct mlx5_dev_ctx_shared *sh = priv->sh;
-
-	if (!priv->config.dv_flow_en) {
-		if (fputs("device dv flow disabled\n", file) <= 0)
-			return -errno;
-		return -ENOTSUP;
-	}
-	return mlx5_devx_cmd_flow_dump(sh->fdb_domain, sh->rx_domain,
-				       sh->tx_domain, file);
-}
-
-int
-mlx5_flow_dump_rule(struct rte_eth_dev *dev, struct rte_flow *flow_idx,
+mlx5_flow_dev_dump(struct rte_eth_dev *dev, struct rte_flow *flow_idx,
 			FILE *file,
 			struct rte_flow_error *error __rte_unused)
 {
 	struct mlx5_priv *priv = dev->data->dev_private;
+	struct mlx5_dev_ctx_shared *sh = priv->sh;
 	uint32_t handle_idx;
 	int ret;
 	struct mlx5_flow_handle *dh;
-	struct rte_flow *flow = mlx5_ipool_get(priv->sh->ipool
-			[MLX5_IPOOL_RTE_FLOW], (uintptr_t)(void *)flow_idx);
+	struct rte_flow *flow;
 
 	if (!priv->config.dv_flow_en) {
 		if (fputs("device dv flow disabled\n", file) <= 0)
@@ -7680,8 +7664,17 @@ mlx5_flow_dump_rule(struct rte_eth_dev *dev, struct rte_flow *flow_idx,
 		return -ENOTSUP;
 	}
 
+	/* dump all */
+	if (!flow_idx)
+		return mlx5_devx_cmd_flow_dump(sh->fdb_domain,
+					sh->rx_domain,
+					sh->tx_domain, file);
+	/* dump one */
+	flow = mlx5_ipool_get(priv->sh->ipool
+		    [MLX5_IPOOL_RTE_FLOW], (uintptr_t)(void *)flow_idx);
 	if (!flow)
 		return -ENOENT;
+
 	handle_idx = flow->dev_handles;
 	while (handle_idx) {
 		dh = mlx5_ipool_get(priv->sh->ipool[MLX5_IPOOL_MLX5_FLOW],

commit 949ac5dbbf365094bb8a62b12fdaefa8fad71520

net/mlx5: fix empty flow dump file
Currently, the flow dump file is closed after socket send. Since
stream file has internal stream buffer, the data written to the
flow dump file descriptor can be still in the stream buffer which
caused flow parser process get the empty flow dump file.

This commit adds an explicitly fflush() to the flow dump file
to make sure the flow dump be written to the file.

Fixes: 53ce799e73b4 ("net/mlx5: backport from upstream for single flow dump")

Signed-off-by: Suanming Mou <suanmingm@nvidia.com>

drivers/net/mlx5/linux/mlx5_socket.c
@@ -107,6 +107,7 @@ mlx5_pmd_socket_handle(void *cb __rte_unused)
 		ret = mlx5_flow_dev_dump(dev,
 			(struct rte_flow *)((uintptr_t)flow_ptr), file, &err);
 
+	fflush(file);
 	/* Set-up the ancillary data and reply. */
 	msg.msg_controllen = 0;
 	msg.msg_control = NULL;