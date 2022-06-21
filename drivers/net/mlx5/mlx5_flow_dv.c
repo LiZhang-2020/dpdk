LiZhang-2020/dpdk

commit a40c92332169a5c1dd4d1733eb4995ce0e6d6452

net/mlx5: skip validation on pop VLAN action
Commit [1] skipped validation on some actions. Use the same mechanism to
skip validation for pop VLAN as well.

[1] 39d9c5167521 ("net/mlx5: skip validation on modify action")

Signed-off-by: Eli Britstein <elibr@nvidia.com>

drivers/net/mlx5/mlx5_flow_dv.c
@@ -1675,8 +1675,9 @@ flow_dv_validate_action_pop_vlan(struct rte_eth_dev *dev,
 					  "cannot pop vlan after decap without "
 					  "match on inner vlan in the flow");
 	/* Pop VLAN without preceding Decap requires outer header with VLAN. */
-	if (!(action_flags & MLX5_FLOW_ACTION_DECAP) &&
-	    !(item_flags & MLX5_FLOW_LAYER_OUTER_VLAN))
+	if (priv->sh->config.dv_validate_mod &&
+	    ((action_flags & MLX5_FLOW_ACTION_DECAP) &&
+	     !(item_flags & MLX5_FLOW_LAYER_OUTER_VLAN)))
 		return rte_flow_error_set(error, ENOTSUP,
 					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
 					  NULL,

commit fa925be2406986e57685001abfb87fa39d80fbfe

net/mlx5: fix VLAN validation skip
Skip VLAN pop validation both for outer and inner VLAN headers

Fixes: a40c92332169 ("net/mlx5: skip validation on pop VLAN action")

Signed-off-by: Eli Britstein <elibr@nvidia.com>
Signed-off-by: Gregory Etelson <getelson@nvidia.com>

drivers/net/mlx5/mlx5_flow_dv.c
@@ -1645,23 +1645,28 @@ flow_dv_validate_action_pop_vlan(struct rte_eth_dev *dev,
 					  RTE_FLOW_ERROR_TYPE_ACTION, action,
 					  "no support for multiple VLAN "
 					  "actions");
-	/* Pop VLAN with preceding Decap requires inner header with VLAN. */
-	if ((action_flags & MLX5_FLOW_ACTION_DECAP) &&
-	    !(item_flags & MLX5_FLOW_LAYER_INNER_VLAN))
-		return rte_flow_error_set(error, ENOTSUP,
-					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
-					  NULL,
-					  "cannot pop vlan after decap without "
-					  "match on inner vlan in the flow");
-	/* Pop VLAN without preceding Decap requires outer header with VLAN. */
-	if (priv->sh->config.dv_validate_mod &&
-	    ((action_flags & MLX5_FLOW_ACTION_DECAP) &&
-	     !(item_flags & MLX5_FLOW_LAYER_OUTER_VLAN)))
-		return rte_flow_error_set(error, ENOTSUP,
-					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
-					  NULL,
-					  "cannot pop vlan without a "
-					  "match on (outer) vlan in the flow");
+	if (priv->sh->config.dv_validate_mod) {
+		/*
+		 * Pop VLAN with preceding Decap
+		 * requires inner header with VLAN.
+		 */
+		if ((action_flags & MLX5_FLOW_ACTION_DECAP)
+		    && !(item_flags & MLX5_FLOW_LAYER_INNER_VLAN))
+			return rte_flow_error_set
+				(error, ENOTSUP,
+				 RTE_FLOW_ERROR_TYPE_UNSPECIFIED, NULL,
+				 "cannot pop vlan after decap without match on inner vlan in the flow");
+		/*
+		 * Pop VLAN without preceding Decap
+		 * requires outer header with VLAN.
+		 */
+		if (!(action_flags & MLX5_FLOW_ACTION_DECAP)
+		    && !(item_flags & MLX5_FLOW_LAYER_OUTER_VLAN))
+			return rte_flow_error_set
+				(error, ENOTSUP,
+				 RTE_FLOW_ERROR_TYPE_UNSPECIFIED, NULL,
+				 "cannot pop vlan without a match on (outer) vlan in the flow");
+	}
 	if (action_flags & MLX5_FLOW_ACTION_PORT_ID)
 		return rte_flow_error_set(error, EINVAL,
 					  RTE_FLOW_ERROR_TYPE_ACTION, action,