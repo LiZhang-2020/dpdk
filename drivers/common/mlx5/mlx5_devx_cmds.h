fc875d72f12c669f5ad1a306bb23e94a92e87954
Dariusz Sosnowski <dsosnowski@nvidia.com>
2022-06-23 14:03:50 +0300

    common/mlx5: add hairpin RQ buffer type capabilities
    This patch adds new HCA capability related to hairpin RQs. This new
    capability, hairpin_data_buffer_locked, indicates whether HCA supports
    locking data buffer of hairpin RQ in ICMC (Interconnect Context Memory
    Cache).
    
    Struct used to define RQ configuration (RQ context) is extended with
    hairpin_data_buffer_type field, which configures data buffer for hairpin
    RQ. It can take the following values:
    
    - MLX5_RQC_HAIRPIN_DATA_BUFFER_TYPE_UNLOCKED_INTERNAL_BUFFER - hairpin
      RQ's data buffer is stored in unlocked memory in ICMC.
    - MLX5_RQC_HAIRPIN_DATA_BUFFER_TYPE_LOCKED_INTERNAL_BUFFER - hairpin
      RQ's data buffer is stored in locked memory in ICMC.
    
    Signed-off-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

--- drivers/common/mlx5/mlx5_devx_cmds.h
+++ drivers/common/mlx5/mlx5_devx_cmds.h
@@ -188,6 +188,7 @@ struct mlx5_hca_attr {
 	uint32_t log_max_hairpin_num_packets:5;
 	uint32_t hairpin_sq_wqe_bb_size:4;
 	uint32_t hairpin_sq_wq_in_host_mem:1;
+	uint32_t hairpin_data_buffer_locked:1;
 	uint32_t vhca_id:16;
 	uint32_t relaxed_ordering_write:1;
 	uint32_t relaxed_ordering_read:1;
@@ -322,6 +323,7 @@ struct mlx5_devx_create_rq_attr {
 	uint32_t state:4;
 	uint32_t flush_in_error_en:1;
 	uint32_t hairpin:1;
+	uint32_t hairpin_data_buffer_type:3;
 	uint32_t ts_format:2;
 	uint32_t user_index:24;
 	uint32_t cqn:24;
