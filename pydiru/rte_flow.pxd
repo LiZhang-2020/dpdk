LiZhang-2020/dpdk

commit 12dc4a7cb7b227d36a7305a1980526d4b55ce952

pydiru: adjust Tx queue flow due to refactoring
Refactor pydiru infrastructure and tests due to "net/mlx5: refactor
internal SQ match"

Signed-off-by: Edward Srouji <edwards@nvidia.com>

pydiru/rte_flow.pxd
@@ -39,7 +39,7 @@ cdef class RteFlowItemVxlan(PydiruCM):
     cdef pdr.rte_flow_item_vxlan item
 
 cdef class Mlx5RteFlowItemTxQueue(PydiruCM):
-    cdef pdr.mlx5_rte_flow_item_tx_queue item
+    cdef pdr.mlx5_rte_flow_item_sq item
 
 cdef class RteFlowItemTag(PydiruCM):
     cdef pdr.rte_flow_item_tag item

app/test-mlx5-pydiru/test_mlx5dr_fdb.py
@@ -4,7 +4,7 @@
 from .utils import raw_traffic, gen_packet, PacketConsts, create_sipv4_rte_items, \
     create_counter_action, verify_counter, send_packets, create_ipv4_rte_item, create_eth_rte_item, \
     NEW_MAC_STR, ModifyFieldId, ModifyFieldLen
-from pydiru.rte_flow import RteFlowItem, RteFlowItemEthdev, RteFlowItemIpv4, Mlx5RteFlowItemTxQueue,\
+from pydiru.rte_flow import RteFlowItem, RteFlowItemEthdev, RteFlowItemIpv4, Mlx5RteFlowItemSq,\
     RteFlowItemEnd
 from pydiru.providers.mlx5.steering.mlx5dr_action import Mlx5drRuleAction, Mlx5drActionDestTable, \
     Mlx5drActionTemplate
@@ -98,9 +98,9 @@ class Mlx5drFDBTest(PydiruTrafficTestCase):
 
         ib_port = self.vf_to_ib_port(self.vf_name)
         ip_rte_items = create_sipv4_rte_items(PacketConsts.SRC_IP)
-        mask = Mlx5RteFlowItemTxQueue(qp_num=0xffffffff)
-        val = Mlx5RteFlowItemTxQueue(qp_num=self.client.qp.qp_num)
-        rte_flow_item = [RteFlowItem(me.MLX5_RTE_FLOW_ITEM_TYPE_TX_QUEUE, val, mask),
+        mask = Mlx5RteFlowItemSq(qp_num=0xffffffff)
+        val = Mlx5RteFlowItemSq(qp_num=self.client.qp.qp_num)
+        rte_flow_item = [RteFlowItem(me.MLX5_RTE_FLOW_ITEM_TYPE_SQ, val, mask),
                          RteFlowItemEnd()]
         vf_actions = [[me.MLX5DR_ACTION_TYP_TIR, me.MLX5DR_ACTION_TYP_LAST]]
         pf_actions = [[me.MLX5DR_ACTION_TYP_VPORT, me.MLX5DR_ACTION_TYP_LAST]]
@@ -153,9 +153,9 @@ class Mlx5drFDBTest(PydiruTrafficTestCase):
         Removes the rule.
         """
         rte_items = [create_ipv4_rte_item(src_addr=PacketConsts.SRC_IP)]
-        mask = Mlx5RteFlowItemTxQueue(qp_num=0xffffffff)
-        val = Mlx5RteFlowItemTxQueue(qp_num=self.client.qp.qp_num)
-        rte_items.append(RteFlowItem(me.MLX5_RTE_FLOW_ITEM_TYPE_TX_QUEUE, val, mask))
+        mask = Mlx5RteFlowItemSq(qp_num=0xffffffff)
+        val = Mlx5RteFlowItemSq(qp_num=self.client.qp.qp_num)
+        rte_items.append(RteFlowItem(me.MLX5_RTE_FLOW_ITEM_TYPE_SQ, val, mask))
         rte_items.append(RteFlowItemEnd())
         self.tmp_rule = Mlx5drRule(self.server.matcher, 0, rte_items, at_idx, actions, len(actions),
                                    Mlx5drRuleAttr(user_data=bytes(8)), self.server.dr_ctx)
@@ -175,9 +175,9 @@ class Mlx5drFDBTest(PydiruTrafficTestCase):
         ib_port = self.vf_to_ib_port(self.vf_name)
         eth_rte_items = [create_eth_rte_item(), RteFlowItemEnd()]
         src_ip_rte_items = [create_ipv4_rte_item(src_addr=PacketConsts.SRC_IP)]
-        mask = Mlx5RteFlowItemTxQueue(qp_num=0xffffffff)
-        val = Mlx5RteFlowItemTxQueue(qp_num=self.client.qp.qp_num)
-        qpn_rte_item = RteFlowItem(me.MLX5_RTE_FLOW_ITEM_TYPE_TX_QUEUE, val, mask)
+        mask = Mlx5RteFlowItemSq(qp_num=0xffffffff)
+        val = Mlx5RteFlowItemSq(qp_num=self.client.qp.qp_num)
+        qpn_rte_item = RteFlowItem(me.MLX5_RTE_FLOW_ITEM_TYPE_SQ, val, mask)
         src_ip_rte_items.append(qpn_rte_item)
         src_ip_rte_items.append(RteFlowItemEnd())
         dst_ip_items = [create_ipv4_rte_item(dst_addr=PacketConsts.DST_IP), RteFlowItemEnd()]

pydiru/libpydiru.pxd
@@ -153,7 +153,7 @@ cdef extern  from '../../../../lib/librte_ethdev/rte_flow.h':
 
 cdef extern  from '../../../../drivers/net/mlx5/mlx5_flow.h':
 
-    cdef struct mlx5_rte_flow_item_tx_queue:
+    cdef struct mlx5_rte_flow_item_sq:
         uint32_t queue
 
     cdef struct rte_flow_item_tag:

pydiru/providers/mlx5/steering/mlx5dr_enums_c.pxd
@@ -68,7 +68,7 @@ cdef extern  from '../../../../drivers/net/mlx5/mlx5_flow.h':
     cpdef enum mlx5_rte_flow_item_type:
         MLX5_RTE_FLOW_ITEM_TYPE_END
         MLX5_RTE_FLOW_ITEM_TYPE_TAG
-        MLX5_RTE_FLOW_ITEM_TYPE_TX_QUEUE
+        MLX5_RTE_FLOW_ITEM_TYPE_SQ
         MLX5_RTE_FLOW_ITEM_TYPE_VLAN
         MLX5_RTE_FLOW_ITEM_TYPE_TUNNEL
 

pydiru/rte_flow.pyx
@@ -164,10 +164,10 @@ cdef class RteFlowItemVxlan(PydiruCM):
         self.item.flags = flags
 
 
-cdef class Mlx5RteFlowItemTxQueue(PydiruCM):
+cdef class Mlx5RteFlowItemSq(PydiruCM):
     def __init__(self, qp_num=0):
         """
-        Initializes a Mlx5RteFlowItemTxQueue object representing mlx5_rte_flow_item_tx_queue
+        Initializes a Mlx5RteFlowItemSq object representing mlx5_rte_flow_item_sq
         C struct.
         :param qp_num: Number of TX queue
         """
@@ -226,8 +226,8 @@ cdef class RteFlowItem(PydiruCM):
             size = sizeof(pdr.rte_flow_item_ethdev)
         if flow_item_type == e.RTE_FLOW_ITEM_TYPE_VXLAN:
             size = sizeof(pdr.rte_flow_item_vxlan)
-        if flow_item_type == me.MLX5_RTE_FLOW_ITEM_TYPE_TX_QUEUE:
-            size = sizeof(pdr.mlx5_rte_flow_item_tx_queue)
+        if flow_item_type == me.MLX5_RTE_FLOW_ITEM_TYPE_SQ:
+            size = sizeof(pdr.mlx5_rte_flow_item_sq)
         if flow_item_type == e.RTE_FLOW_ITEM_TYPE_VLAN:
             size = sizeof(pdr.rte_flow_item_vlan)
         if flow_item_type in [e.RTE_FLOW_ITEM_TYPE_TAG,

commit 4caab361d8bbae5b9420371a5e4a250060fbf66f

pydiru: fix class name in pxd file
Fix class name in pxd file to suit the pyx class name.

Fixes: 12dc4a7cb7b2 ("pydiru: adjust Tx queue flow due to refactoring")
Signed-off-by: Daria Velikovsky <daria@nvidia.com>
Signed-off-by: Edward Srouji <edwards@nvidia.com>

pydiru/rte_flow.pxd
@@ -38,7 +38,7 @@ cdef class RteFlowItemEthdev(PydiruCM):
 cdef class RteFlowItemVxlan(PydiruCM):
     cdef pdr.rte_flow_item_vxlan item
 
-cdef class Mlx5RteFlowItemTxQueue(PydiruCM):
+cdef class Mlx5RteFlowItemSq(PydiruCM):
     cdef pdr.mlx5_rte_flow_item_sq item
 
 cdef class RteFlowItemTag(PydiruCM):